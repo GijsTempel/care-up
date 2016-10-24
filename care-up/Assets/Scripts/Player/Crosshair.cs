9b0896daf252f2b6b16d312dce7df8fe2621842d
Alexandr Zhytaryuk <ozhytariuk@sharpminds.com>
2016-10-24 17:29:19 +0300

    Added camera movement, crosshair and basic player movement

---
+++ care-up/Assets/Scripts/Player/Crosshair.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+    public Texture2D crosshairTexture;
+
+    private Rect position;
+
+	void OnGUI () {
+
+        if (crosshairTexture == null)
+            return;
+
+        position = new Rect((Screen.width - crosshairTexture.width) / 2.0f,
+                           (Screen.height - crosshairTexture.height) / 2.0f,
+                           crosshairTexture.width, crosshairTexture.height);
+
+        GUI.DrawTexture(position, crosshairTexture);
+
+	}
+}
---
+++ care-up/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour {
+
+    [Serializable]
+    public class MovementSettings
+    {
+        public float ForwardSpeed = 8.0f;   // Speed when walking forward
+        public float BackwardSpeed = 4.0f;  // Speed when walking backwards
+        public float StrafeSpeed = 4.0f;    // Speed when walking sideways
+        [HideInInspector] public float CurrentTargetSpeed = 8f;
+
+        public void UpdateDesiredTargetSpeed(Vector2 input)
+        {
+            if (input == Vector2.zero) return;
+            if (input.x > 0 || input.x < 0)
+            {
+                //strafe
+                CurrentTargetSpeed = StrafeSpeed;
+            }
+            if (input.y < 0)
+            {
+                //backwards
+                CurrentTargetSpeed = BackwardSpeed;
+            }
+            if (input.y > 0)
+            {
+                //forwards
+                //handled last as if strafing and moving forward at the same time forwards speed should take precedence
+                CurrentTargetSpeed = ForwardSpeed;
+            }
+        }
+    }
+
+    public Camera mainCamera;
+    public MovementSettings movementSettings = new MovementSettings();
+    public CameraMovement cameraMovement = new CameraMovement();
+
+    private CharacterController controller;
+
+	void Start () {
+
+        controller = GetComponent<CharacterController>();
+
+        cameraMovement.Init(transform, mainCamera.transform);
+
+	}
+
+    void Update() {
+        //avoids the mouse looking if the game is effectively paused
+        if (Mathf.Abs(Time.timeScale) < float.Epsilon) return;
+
+        // get the rotation before it's changed
+        float oldYRotation = transform.eulerAngles.y;
+
+        cameraMovement.Update(transform, mainCamera.transform);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 inputVector = new Vector2
+        {
+            x = Input.GetAxis("Horizontal"),
+            y = Input.GetAxis("Vertical")
+        };
+        
+        movementSettings.UpdateDesiredTargetSpeed(inputVector);
+
+        Vector3 movementVector = mainCamera.transform.forward * inputVector.y +
+                                 mainCamera.transform.right * inputVector.x;
+        movementVector *= movementSettings.CurrentTargetSpeed / 100.0f;
+        movementVector.y = 0.0f; 
+
+        controller.Move(movementVector);
+	}
+}
