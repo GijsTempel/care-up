21f040c8992217187a28a880dcabb94efda2de8a
Alexandr Zhytaryuk <ozhytariuk@sharpminds.com>
2016-11-14 14:36:48 +0200

    Added basic AnimationManager script, added test xmls for injectionScene

---
+++ care-up/Assets/Scripts/Player/AnimationManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationManager : MonoBehaviour {
+
+    private Animation anim;
+
+    public void PlayAnimation(string name)
+    {
+        anim.Play(name, PlayMode.StopSameLayer);
+    }
+}
