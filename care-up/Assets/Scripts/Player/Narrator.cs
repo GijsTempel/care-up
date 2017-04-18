883dd8b0f2ba73ac6b2d63e250abdaac91fec3a5
Alexander Zhytaryuk <olexandrzhytaryuk@gmail.com>
2017-04-18 16:02:53 +0300

    narrator upd

--- care-up/Assets/Scripts/Player/Narrator.cs
+++ care-up/Assets/Scripts/Player/Narrator.cs
@@ -42,7 +42,7 @@ public class Narrator : MonoBehaviour {
             }
             else
             {
-                AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
+                audioSource.PlayOneShot(clip);
             }
             return true;
         }
