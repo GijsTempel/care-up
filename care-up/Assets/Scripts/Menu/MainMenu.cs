2b9b4a52fca28784c05db379497e3696ce328fed
Alexander Zhytaryuk <olexandrzhytaryuk@gmail.com>
2017-06-21 13:34:02 +0300

    UI version of MainMenu

---
+++ care-up/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    
+    private LoadingScreen loadingScreen;
+
+    private void Start()
+    {
+        if (GameObject.Find("Preferences") != null)
+        {
+            loadingScreen = GameObject.Find("Preferences").GetComponent<LoadingScreen>();
+            if (loadingScreen == null) Debug.LogError("No loading screen found");
+        }
+        else
+        {
+            Debug.LogWarning("No 'preferences' found. Game needs to be started from first scene");
+        }
+    }
+
+    public void OnStartButtonClick()
+    {
+        loadingScreen.LoadLevel("Tutorial");
+    }
+
+    public void OnQuitButtonClick()
+    {
+        Application.Quit();
+    }
+}
