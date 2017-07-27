9ba073bc11b0707de4d453a2b9bcc0d06b6b94c8
Goizz <gijstempel@msn.com>
2017-07-27 17:01:01 +0100

    UI changes in: Main menu Selection menu

---
+++ care-up/Assets/Scripts/Menu/SceneSelectionManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSelectionManager : MonoBehaviour {
+
+    public SceneSelection CurrentMenu;
+
+    public void Start()
+    {
+        ShowMenu(CurrentMenu);
+    }
+
+    public void ShowMenu (SceneSelection sceneselection)
+    {
+        if (CurrentMenu != null)
+        {
+            CurrentMenu.IsOpen = false;
+
+            CurrentMenu = sceneselection;
+            CurrentMenu.IsOpen = true;
+        }
+    }
+
+}
