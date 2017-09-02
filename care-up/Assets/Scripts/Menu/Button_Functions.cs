99d04a58feb0c2845f2e2848696d115ac37f6199
TripleMotion Media <gtempel@triplemotion.nl>
2017-09-02 10:17:25 +0200

    - Added button script for sounds - Added sounds to button 	- Menu 	-Scene selection

---
+++ care-up/Assets/Scripts/Menu/Button_Functions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Functions : MonoBehaviour {
+
+	public AudioClip buttonHover;
+	public AudioClip buttonClick;
+	private AudioSource source;
+
+	void Awake()
+	{
+		source = GetComponent<AudioSource> ();
+	}
+	public void OnButtonHover()
+	{
+		source.PlayOneShot (buttonHover, 0.5F);
+	}
+	public void OnButtonClick()
+	{
+		source.PlayOneShot (buttonClick, 0.15F);
+	}
+
+}
