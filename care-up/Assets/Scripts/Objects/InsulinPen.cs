068c4ddc1f6195cc271b544debf2512e6d53599b
Alexander Zhytaryuk <olexandrzhytaryuk@gmail.com>
2017-11-01 17:24:07 +0200

    insulin scene xml updates, insulin pen prefab set up

---
+++ care-up/Assets/Scripts/Objects/InsulinPen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsulinPen : PickableObject {
+
+    public bool animateButton = false;
+
+    private Transform button;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        animateButton = false;
+        button = transform.Find("insulinPenButton");
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (animateButton)
+        {
+            button.localPosition = new Vector3(
+                button.localPosition.x,
+                button.localPosition.y,
+                -0.002f * controlBone.localPosition.y);
+
+            button.localRotation = Quaternion.Euler(
+                button.localRotation.eulerAngles.x,
+                button.localRotation.eulerAngles.y,
+                -14.5f * controlBone.localPosition.y);
+        }
+    }
+
+}
