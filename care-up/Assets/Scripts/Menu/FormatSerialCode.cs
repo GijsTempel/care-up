89e9d9f4270411f20af84958b2d46121df02fb2e
Alexander Zhytaryuk <olexandrzhytaryuk@gmail.com>
2017-12-20 13:19:51 +0200

    format serial code

---
+++ care-up/Assets/Scripts/Menu/FormatSerialCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+[RequireComponent(typeof(InputField))]
+public class FormatSerialCode : MonoBehaviour {
+
+    private InputField input;
+
+    private void Start()
+    {
+        input = GetComponent<InputField>();
+    }
+
+    public void Format()
+    {
+        input.text = Regex.Replace(input.text, @"\s+", string.Empty);
+    }
+}
