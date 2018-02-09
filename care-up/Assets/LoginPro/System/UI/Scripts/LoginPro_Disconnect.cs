c42608a822de0ae14c6f007840546fdf6d70ebe7
DESKTOP-7KILOFR\Gebruiker <gtempel@triplemotion.nl>
2018-02-09 16:43:39 +0100

    Removed some aniamtions in code which were obsolete

--- care-up/Assets/LoginPro/System/UI/Scripts/LoginPro_Disconnect.cs
+++ care-up/Assets/LoginPro/System/UI/Scripts/LoginPro_Disconnect.cs
@@ -22,29 +22,5 @@ namespace LoginProAsset
             // StartCoroutine(LaunchRegisterAnimations());
         }
 
-        /*private IEnumerator LaunchRegisterAnimations()
-        {
-            // Hide report window
-            if (this.AnimationHideReports != null)
-                yield return this.AnimationHideReports.Launch();
-
-            // Hide friends window
-            if (this.AnimationHideFriends != null)
-                yield return this.AnimationHideFriends.Launch();
-
-            // Hide current window
-            if (this.AnimationHideCurrentWindow != null)
-                yield return this.AnimationHideCurrentWindow.Launch();
-
-            // Show message on success
-            if (this.AnimationShowMessage != null)
-                yield return this.AnimationShowMessage.Show("U bent uitgelogd.", 2);
-
-            // Launch animation on success
-            if (this.AnimationShowLogin != null)
-                yield return this.AnimationShowLogin.Launch();
-
-            yield return null;
-        }*/
     }
 }
