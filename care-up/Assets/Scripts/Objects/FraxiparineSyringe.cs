209769c04983e0c23064fa59c196060fb748e485
Alexander <olexandrzhytaryuk@gmail.com>
2018-04-13 17:10:59 +0300

    frax syringe stuff

---
+++ care-up/Assets/Scripts/Objects/FraxiparineSyringe.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FraxiparineSyringe : PickableObjectWithInfo
+{
+
+    public bool updatePlunger = false;
+    public bool updateTube = false;
+
+    private Transform plunger;
+    private Transform tube;
+
+    public override void SaveInfo(ref Vector3 left, ref Vector3 right)
+    {
+        left = new Vector3(
+            (updatePlunger ? 1.0f : 0.0f),
+            Mathf.InverseLerp(-0.013f, 0.06f, plunger.localPosition.y),
+            0.0f);
+
+        right = new Vector3(
+            (updateTube ? 1.0f : 0.0f),
+            Mathf.InverseLerp(-0.013f, 0.06f, tube.localPosition.z),
+            0.0f);
+    }
+
+    public override void LoadInfo(Vector3 left, Vector3 right)
+    {
+        updatePlunger = left.x == 1.0f ? true : false;
+
+        if (plunger == null)
+        {
+            plunger = transform.Find("Fraxi_plunger");
+        }
+
+        plunger.localPosition = new Vector3(
+                plunger.localPosition.x,
+                Mathf.Lerp(-0.013f, 0.06f, left.y),
+                plunger.localPosition.z);
+
+        updateTube = right.x == 1.0f ? true : false;
+
+        if (tube == null)
+        {
+            tube = transform.Find("Frexi_tube");
+        }
+
+        tube.localPosition = new Vector3(
+            tube.localPosition.x,
+            Mathf.Lerp(-0.013f, 0.06f, right.y),
+            tube.localPosition.z);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        plunger = transform.Find("Fraxi_plunger");
+        if (plunger == null) Debug.LogError("No plunger found!");
+
+        tube = transform.Find("Frexi_tube");
+        if (tube == null) Debug.LogError("No protecting tube found!");
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (updatePlunger)
+        {
+            plunger.localPosition = new Vector3(
+                plunger.localPosition.x,
+                Mathf.Lerp(-0.013f, 0.06f, leftControlBone.localPosition.y),
+                plunger.localPosition.z);
+        }
+
+        if (updateTube)
+        {
+            if (tube != null)
+            {
+                tube.localPosition = new Vector3(
+                    plunger.localPosition.x,
+                    plunger.localPosition.y,
+                    Mathf.Lerp(-0.013f, 0.06f, leftControlBone.localPosition.z));
+            }
+        }
+    }
+
+    public override bool Use(bool hand = false, bool noTarget = false)
+    {
+        tutorial_usedOn = true;
+        string[] info = actionManager.CurrentUseOnInfo;
+
+        if (controls.SelectedObject != null && controls.CanInteract)
+        {
+        }
+        else // cannot interact or target == ""
+        {
+        }
+
+        actionManager.OnUseOnAction(name, controls.SelectedObject != null ? controls.SelectedObject.name : "");
+
+        return (info[0] == name && controls.SelectedObject != null && info[1] == controls.SelectedObject.name);
+    }
+}
---
+++ care-up/Assets/Scripts/Player/AnimationScripts/FraxiparineTubeAnimation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FraxiparineTubeAnimation : AnimationUseOn
+{
+    public bool hand;
+
+    public int tubeStart;
+    public int tubeEnd;
+
+    private float frame = 0f;
+    private float prevFrame = 0f;
+
+    private FraxiparineSyringe syringe;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+
+        syringe = hand ? inv.LeftHandObject.GetComponent<FraxiparineSyringe>() : inv.RightHandObject.GetComponent<FraxiparineSyringe>();
+
+        frame = 0f;
+        prevFrame = 0f;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (PlayerAnimationManager.CompareFrames(frame, prevFrame, tubeStart))
+        {
+            syringe.updateTube = true;
+        }
+
+        if (PlayerAnimationManager.CompareFrames(frame, prevFrame, tubeEnd))
+        {
+            syringe.updateTube = false;
+        }
+
+        if (animator.speed != 0)
+        {
+            prevFrame = frame;
+            frame += Time.deltaTime;
+        }
+
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+
+        syringe.updateTube = false;
+    }
+}
