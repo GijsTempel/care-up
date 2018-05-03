29e0bd555feb11d554f6cbeffb4babcf32423caf
Alexander <olexandrzhytaryuk@gmail.com>
2018-05-03 12:47:13 +0300

    sort by time too

--- care-up/Assets/LoginPro/Upload my content/LoginPro_Server/Game/Includes/Actions/GetSceneLeaders.php
+++ care-up/Assets/LoginPro/Upload my content/LoginPro_Server/Game/Includes/Actions/GetSceneLeaders.php
@@ -3,7 +3,7 @@
 $scene  = $datas[0];
 $number = $datas[1];
 
-$query = "SELECT ".$_SESSION['AccountTable'].".username, ".$_SESSION['HighScores'].".score, ".$_SESSION['HighScores'].".time FROM ".$_SESSION['HighScores']." INNER JOIN ".$_SESSION['AccountTable']." ON ".$_SESSION['HighScores'].".account_id = ".$_SESSION['AccountTable'].".id WHERE scene = :scene ORDER BY score DESC LIMIT :number";
+$query = "SELECT ".$_SESSION['AccountTable'].".username, ".$_SESSION['HighScores'].".score, ".$_SESSION['HighScores'].".time FROM ".$_SESSION['HighScores']." INNER JOIN ".$_SESSION['AccountTable']." ON ".$_SESSION['HighScores'].".account_id = ".$_SESSION['AccountTable'].".id WHERE scene = :scene ORDER BY score DESC, time LIMIT :number";
 $parameters = array(':scene' => $scene, ':number' => $number);
 $stmt = ExecuteQuery($query, $parameters);
 
