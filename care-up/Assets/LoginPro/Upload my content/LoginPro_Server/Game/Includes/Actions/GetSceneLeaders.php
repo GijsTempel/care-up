7882dc8eb46a999d8f2728b5c86bccf9839593ae
Alexander <olexandrzhytaryuk@gmail.com>
2018-05-03 07:42:20 +0300

    get leaderboard php

---
+++ care-up/Assets/LoginPro/Upload my content/LoginPro_Server/Game/Includes/Actions/GetSceneLeaders.php
@@ -0,0 +1,21 @@
+<?php
+
+$scene  = $datas[0];
+$number = $datas[1];
+
+$query = "SELECT ".$_SESSION['AccountTable'].".username, ".$_SESSION['HighScores'].".score, ".$_SESSION['HighScores'].".time FROM ".$_SESSION['HighScores']." INNER JOIN ".$_SESSION['AccountTable']." ON ".$_SESSION['HighScores'].".account_id = ".$_SESSION['AccountTable'].".id WHERE scene = :scene ORDER BY score DESC LIMIT :number";
+$parameters = array(':scene' => $scene, ':number' => $number);
+$stmt = ExecuteQuery($query, $parameters);
+
+$dataToGetFromServer = array();
+
+while ($row = $stmt->fetch())
+{
+	$dataToGetFromServer[] = $row["username"];
+	$dataToGetFromServer[] = $row["score"];
+	$dataToGetFromServer[] = $row["time"];
+}
+
+sendArrayAndFinish($dataToGetFromServer);
+
+?>
