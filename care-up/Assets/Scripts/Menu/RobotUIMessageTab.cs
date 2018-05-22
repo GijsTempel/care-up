ef6ae8c7256f570d0aa3742bed7ce27369e8cb1f
Alexander <olexandrzhytaryuk@gmail.com>
2018-05-22 06:39:36 +0300

    message center template

---
+++ care-up/Assets/Scripts/Menu/RobotUIMessageTab.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotUIMessageTab : RobotUITabs
+{
+    private List<GameObject> list;
+    private Transform itemsParent;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        list = new List<GameObject>();
+        itemsParent = transform.Find("GeneralDynamicCanvas");
+    }
+
+    public void NewMessage(string text)
+    {
+        GameObject newObject = new GameObject("Item");
+        newObject.transform.parent = itemsParent;
+
+        newObject.AddComponent<Text>();
+        newObject.GetComponent<Text>().text = text;
+
+        // add script that controls if message seen?
+        // newObject.AddComponent<blabla>();
+        // newObject.GetComponent<blabla>().seen = false;
+    }
+}
