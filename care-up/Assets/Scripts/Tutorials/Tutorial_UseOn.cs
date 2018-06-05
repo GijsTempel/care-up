94d8d9428bd503c8a4bdf45fd31a0a46056a1ae3
Alexander <olexandrzhytaryuk@gmail.com>
2018-06-05 04:19:07 +0300

    use on tutorial + new unity version

---
+++ care-up/Assets/Scripts/Tutorials/Tutorial_UseOn.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_UseOn : TutorialManager {
+
+    public enum TutorialStep
+    {
+        First,
+        Welcome,
+        PickNeedle,
+        UseOn,
+        Done,
+        None
+    }
+
+    private TutorialStep currentStep = TutorialStep.First;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (!Paused())
+        {
+            switch(currentStep)
+            {
+                case TutorialStep.First:
+                    currentStep = TutorialStep.Welcome;
+                    UItext.text = "Welcome. This tutorial will teach you how to use items on other items that are on the table.";
+                    SetUpTutorialNextButton();
+
+                    GameObject.Find("DevHint").SetActive(false);
+                    break;
+                case TutorialStep.Welcome:
+                    if (nextButtonClicked)
+                    {
+                        currentStep = TutorialStep.PickNeedle;
+                        UItext.text = "Come close to items. Pick the needle.";
+
+                        handsInventory.tutorial_pickedLeft = false;
+                        
+                        itemToPick = "AbsorptionNeedleNoCap";
+
+                        particleHint.SetActive(true);
+                        particleHint.transform.position = GameObject.Find("AbsorptionNeedleNoCap").transform.position;
+                    }
+                    break;
+                case TutorialStep.PickNeedle:
+                    if (handsInventory.tutorial_pickedLeft)
+                    {
+                        handsInventory.tutorial_pickedLeft = false;
+                        handsInventory.tutorial_pickedRight = false;
+                        itemToPick = "";
+
+                        particleHint.SetActive(false);
+
+                        currentStep = TutorialStep.UseOn;
+                        UItext.text = "Use it on the needle cup.";
+                        
+                        handsInventory.tutorial_itemUsedOn = false;
+                    }
+                    break;
+                case TutorialStep.UseOn:
+                    if (handsInventory.tutorial_itemUsedOn)
+                    {
+                        handsInventory.tutorial_itemUsedOn = false;
+                        currentStep = TutorialStep.Done;
+                        UItext.text = "Great work! This concludes picking up tutorial.";
+                    }
+                    break;
+                case TutorialStep.Done:
+                    currentStep = TutorialStep.None;
+                    endPanel.SetActive(true);
+                    player.enabled = false;
+                    GameObject.FindObjectOfType<RobotManager>().enabled = false;
+                    foreach (InteractableObject o in GameObject.FindObjectsOfType<InteractableObject>())
+                    {
+                        o.Reset();
+                        o.enabled = false;
+                    }
+                    break;
+            }
+        }
+    }
+}
