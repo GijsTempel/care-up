9e53ea7356c3c3c728a841f46d57dd2909269de8
Alexander <olexandrzhytaryuk@gmail.com>
2018-06-27 23:37:45 +0300

    cant talk until correct step

--- care-up/Assets/Scripts/Tutorials/Tutorial_Talk.cs
+++ care-up/Assets/Scripts/Tutorials/Tutorial_Talk.cs
@@ -35,21 +35,17 @@ public class Tutorial_Talk : TutorialManager
             switch (currentStep)
             {
                 case TutorialStep.First:
+                    GameObject.FindObjectOfType<InjectionPatient>().allowToTalk = false;
+
                     currentStep = TutorialStep.Welcome;
-                    //hintsBox.anchoredPosition = new Vector2(-14.8f, 9.9f);
-                    //hintsBox.sizeDelta = new Vector2(788f, 524.9f);
 					hintsN.SetSize(788f, 524.9f);
                     hintsN.LockTo("UI(Clone)", new Vector3(-393.80f, 214.70f, 0.00f));
                     UItext.text = "Welkom. In deze leermodule zul je leren hoe je met mensen een gesprek kunt aangaan.";
                     SetUpTutorialNextButton();
-
-     
                     break;
                 case TutorialStep.Welcome:
                     if (nextButtonClicked)
                     {
-						//hintsBox.anchoredPosition = new Vector2(584f, -139f);
-						//hintsBox.sizeDelta = new Vector2(472.5f, 298.9f);
 						hintsN.ResetSize();
 						hintsN.LockTo("Patient", new Vector3(0.00f, 0.00f, 0.00f));
                         currentStep = TutorialStep.MoveTo;
@@ -62,11 +58,11 @@ public class Tutorial_Talk : TutorialManager
                     if (player.tutorial_movedTo)
                     {
                         currentStep = TutorialStep.OpenOptions;
-                        //hintsBox.anchoredPosition = new Vector2(731f, -253f);
 						hintsN.LockTo("RightShoulder", new Vector3(0.00f, 0.00f, 0.00f));
                         UItext.text = "Heel goed. Klik nu nogmaals op de cliënt om het een gesprek te starten. ";
 
                         patient.tutorial_used = false;
+                        GameObject.FindObjectOfType<InjectionPatient>().allowToTalk = true;
                     }
                     break;
                 case TutorialStep.OpenOptions:
@@ -75,7 +71,6 @@ public class Tutorial_Talk : TutorialManager
                         patient.tutorial_used = false;
 
                         currentStep = TutorialStep.Talk;
-                        //hintsBox.anchoredPosition = new Vector2(-261f, 228f);
 						hintsN.LockTo("SelectionDialogue(Clone)", new Vector3(-326.15f, 124.90f, 0.00f));
 						hintsN.SetIconPosition(1);
                         UItext.text = "Als je een gesprek start kun je keuzes maken over wat je tegen de persoon wilt zeggen. Laten we de cliënt begroeten door de optie 'Goedemorgen' te kiezen.";
@@ -87,7 +82,6 @@ public class Tutorial_Talk : TutorialManager
                     if (patient.tutorial_talked)
                     {
                         patient.tutorial_talked = false;
-                        //hintsBox.anchoredPosition = new Vector2(532f, -318f);
 						hintsN.SetIconPosition(0);
 						hintsN.LockTo("SceneLoader 1", new Vector3(356.74f, 39.10f, 0.00f));
                         currentStep = TutorialStep.Done;
