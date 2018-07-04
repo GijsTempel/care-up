6943d549c453bb7b78ee25326fa1f418b6fbd8fc
Acvarium <vshmorgun@yandex.ru>
2018-07-04 14:39:20 +0300

    Return correct SceneSelectionManager.cs

--- care-up/Assets/Scripts/Menu/SceneSelectionManager.cs
+++ care-up/Assets/Scripts/Menu/SceneSelectionManager.cs
@@ -7,31 +7,42 @@ public class SceneSelectionManager : MonoBehaviour {
 
     public SceneSelection CurrentMenu;
 
-    private Image practiceButton;
-    private Image testButton;
-    private Text description;
+    public Image practiceButton;
+    public Image testButton;
+    //private Text description;
 
     private PlayerPrefsManager manager;
 
     private string practiceText =
         "Oefenen: Oefen het protocol waarin je elke stap van de werkwijze bovenin het scherm kunt zien. Volg de stappen om ze te oefenen en te leren.";
-    //private string testText =
-        //"Toetsen: Toets je kennis. Tijdens een toets zie je geen hints en moet je de stappen van de protocol uit je hoofd uitvoeren. Zoals in het echt.";
+    private string testText =
+        "Toetsen: Toets je kennis. Tijdens een toets zie je geen hints en moet je de stappen van de protocol uit je hoofd uitvoeren. Zoals in het echt.";
 
     public void Start()
     {
-        manager = GameObject.Find("Preferences").GetComponent<PlayerPrefsManager>();
+        if (GameObject.Find("Preferences") != null)
+        {
+            manager = GameObject.Find("Preferences").GetComponent<PlayerPrefsManager>();
+            if (manager == null) Debug.LogWarning("No prefs manager ( start from 1st scene? )");
+        }
+        else
+        {
+            Debug.LogWarning("No prefs manager ( start from 1st scene? )");
+        }
 
-        ShowMenu(CurrentMenu);
+        //ShowMenu(CurrentMenu);
 
-        practiceButton = GameObject.Find("PracticeButton").GetComponent<Image>();
+        //practiceButton = GameObject.Find("PracticeButton").GetComponent<Image>();
         //testButton = GameObject.Find("TestButton").GetComponent<Image>();
 
-        //practiceButton.color = Color.green;
-        manager.practiceMode = true;
-        
-       description = GameObject.Find("PracticeTestDescription").GetComponent<Text>();
-        description.text = practiceText;
+        practiceButton.color = Color.green;
+        if (manager)
+        {
+            manager.practiceMode = true;
+        }
+
+        //description = GameObject.Find("PracticeTestDescription").GetComponent<Text>();
+        //description.text = practiceText;
     }
 
     public void ShowMenu (SceneSelection sceneselection)
@@ -48,20 +59,26 @@ public class SceneSelectionManager : MonoBehaviour {
     public void OnPracticeButtonClick()
     {
         practiceButton.color = Color.green;
-        //testButton.color = Color.white;
+        testButton.color = Color.white;
 
-        description.text = practiceText;
+        //description.text = practiceText;
 
-        manager.practiceMode = true;
+        if (manager)
+        {
+            manager.practiceMode = true;
+        }
     }
 
-   /* public void OnTestButtonClick()
+    public void OnTestButtonClick()
     {
         practiceButton.color = Color.white;
         testButton.color = Color.green;
 
-        description.text = testText;
+        //description.text = testText;
 
-        manager.practiceMode = false; 
-    }*/
+        if (manager)
+        {
+            manager.practiceMode = false;
+        }
+    }
 }
