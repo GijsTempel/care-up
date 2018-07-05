a8efa3664d9daba2a4b9528855479998b7482206
Acvarium <vshmorgun@yandex.ru>
2018-07-05 16:57:39 +0300

    Added exception for PDF-quality for WebGL build

---
+++ care-up/Assets/Scripts/Cheats/PDFQuality.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PDFQuality : MonoBehaviour {
+
+	Paroxe.PdfRenderer.PDFViewer pdfViewer;
+	      
+	// Use this for initialization
+	void Start () {
+		pdfViewer = gameObject.GetComponent<Paroxe.PdfRenderer.PDFViewer>();
+      
+		#if UNITY_WEBGL
+		pdfViewer.MaxZoomFactorTextureQuality = 1f;
+		#endif
+
+	}
+	
+
+}
