8643bfb82cea9a4e42799f1aebe1dc314e53f4fb
TripleMotion Media <gtempel@triplemotion.nl>
2018-07-08 17:19:42 +0200

    - Connected PDF's to correct scenes - Added in extra explanation for step in insulin XML - Switched to Web GL build using Webassembly instead of asm.js - removed unused StreamingAssets folder

--- care-up/Assets/WebGLTemplates/Modern/TemplateData/UnityProgress.js
+++ care-up/Assets/WebGLTemplates/Modern/TemplateData/UnityProgress.js
@@ -28,10 +28,10 @@ function UnityProgress(gameInstance, progress) {
         return;
     } else if (progress == 1) {
         document.getElementById("ProgressLine").style.width = (200 * (progress)) + "px";
-        document.getElementById("loadingInfo").innerHTML = "loading";
+        document.getElementById("loadingInfo").innerHTML = "Laden...";
         document.getElementById("progressC").innerHTML = Math.floor((100 * progress)) + "%";
     } else {
-        document.getElementById("loadingInfo").innerHTML = "downloading";
+        document.getElementById("loadingInfo").innerHTML = "Laden...";
         document.getElementById("progressC").innerHTML = Math.floor((100 * progress)) + "%";
         document.getElementById("ProgressLine").style.width = (200 * (progress)) + "px";
     }
