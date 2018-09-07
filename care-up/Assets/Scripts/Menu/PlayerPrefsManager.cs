be00cd7b88545ba11967af5f64d1e709daa044c4
Alexander <olexandrzhytaryuk@gmail.com>
2018-09-07 16:52:54 +0300

    fill message on getting activation info

--- care-up/Assets/Scripts/Menu/PlayerPrefsManager.cs
+++ care-up/Assets/Scripts/Menu/PlayerPrefsManager.cs
@@ -187,6 +187,9 @@ public class PlayerPrefsManager : MonoBehaviour
 
         foreach (string data in datas)
         {
+            if (data == "Received")
+                break;
+
             string[] result;
             result = data.Split('|');
 
