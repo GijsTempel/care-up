a2b01a7b55e843610d80a09d03fe56d573e4345f
Alexander <olexandrzhytaryuk@gmail.com>
2018-09-21 09:19:49 +0300

    load menu after log in

---
+++ care-up/Assets/Scripts/Menu/LoadMenuAfterLoginWP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MBS;
+
+public class LoadMenuAfterLoginWP : MonoBehaviour {
+    
+	void Start ()
+    {
+        WULogin.onLoggedIn += LoadMenuScene;
+	}
+
+    void LoadMenuScene( CML response )
+    {
+        Debug.Log("Login was successful. Loading UMenuPro scene.");
+        bl_SceneLoaderUtils.GetLoader.LoadLevel("UMenuPro");
+    }
+}
