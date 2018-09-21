78d4530daeda73eefb373f48adde4f9fc8d7348c
Alexander <olexandrzhytaryuk@gmail.com>
2018-09-21 15:09:45 +0300

    load loginmenu on logout

---
+++ care-up/Assets/Scripts/Menu/LoadStartSceneAfterLogoutWP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MBS;
+
+public class LoadStartSceneAfterLogoutWP : MonoBehaviour {
+    
+	void Start () {
+        WULogin.onLoggedOut += LoadStartScene;
+
+    }
+
+    void LoadStartScene(CML response)
+    {
+        Debug.Log("Logout was successful. Loading first scene.");
+        bl_SceneLoaderUtils.GetLoader.LoadLevel("LoginMenu");
+    }
+}
