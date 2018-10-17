1ef3edbd7a556b34d1e49feae7076a49a18810fc
Alexander <olexandrzhytaryuk@gmail.com>
2018-10-17 03:45:37 +0300

    catheter pack same as workfield

---
+++ care-up/Assets/Scripts/Objects/CatheterPack.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatheterPack : UsableObject
+{
+    public List<GameObject> objects = new List<GameObject>();
+    
+    private bool toggle = false;
+
+    protected override void Start()
+    {
+        base.Start();
+        
+        toggle = false;
+
+        foreach (GameObject obj in objects)
+        {
+            if (obj != null)
+                obj.SetActive(toggle);
+        }
+    }
+
+    public override void Use()
+    {
+        if (!ViewModeActive() && actionManager.CompareUseObject(name))
+        {
+            if (actionManager.CompareUseObject(name))
+            {
+                PlayerAnimationManager.PlayAnimation("Use " + name, transform);
+
+                ToggleObjects();
+            }
+
+            actionManager.OnUseAction(name);
+            controls.ResetObject();
+            Reset();
+        }
+    }
+
+    public void ToggleObjects()
+    {
+        toggle = !toggle;
+        foreach (GameObject obj in objects)
+        {
+            if (obj)
+            {
+                obj.SetActive(toggle);
+            }
+        }
+    }
+}
