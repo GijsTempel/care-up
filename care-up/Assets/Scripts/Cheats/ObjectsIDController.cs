006aaf646c26b3c1f2d7002e198c0e1ae88a4f27
Acvarium <vshmorgun@yandex.ru>
2018-10-31 18:23:58 +0200

    Edited animation. Added animation to the game

--- care-up/Assets/Scripts/Cheats/ObjectsIDController.cs
+++ care-up/Assets/Scripts/Cheats/ObjectsIDController.cs
@@ -58,6 +58,8 @@ public class ObjectsIDController : MonoBehaviour
 		new ObjectsIDs("catheter_bag_packed", 42,0),
 		new ObjectsIDs("catheter_bag_packed_opened", 42,1),
 		new ObjectsIDs("catheter_bag_packed_empty", 42,2),
+		new ObjectsIDs("catheter", 43,0),
+        
     };
 
 
