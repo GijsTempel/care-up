c77ed5cb9853b42b386ffba4bb076806a03c52ae
Acvarium <vshmorgun@yandex.ru>
2018-11-01 16:53:37 +0200

    Separated cinematic

---
+++ care-up/Assets/Scripts/Player/AnimationScripts/Cinematic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cinematic : StateMachineBehaviour
+{
+    public string target;
+    protected CameraMode mode;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        mode = GameObject.Find("GameLogic").GetComponent<CameraMode>();
+        mode.SetCinematicMode(GameObject.Find(target).transform);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        mode.animationEnded = true;
+    }
+
+   
+}
