c116bb959b6e0e23627b050fe864d351cd1966de
Alexander <olexandrzhytaryuk@gmail.com>
2018-11-08 13:50:59 +0200

    separete script for adding ghost positions

---
+++ care-up/Assets/Scripts/Player/AnimationScripts/AddNewGhostPositionsOnAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddNewGhostPositionsOnAnimation : StateMachineBehaviour
+{
+    public List<HandsInventory.GhostPosition> list = new List<HandsInventory.GhostPosition>();
+
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        HandsInventory inv = GameObject.FindObjectOfType<HandsInventory>();
+        
+        foreach (HandsInventory.GhostPosition i in list)
+        {
+            inv.customGhostPositions.Add(i);
+        }
+    }
+}
--- care-up/Assets/Scripts/Player/AnimationScripts/CatherisationPutClothUnder.cs
+++ care-up/Assets/Scripts/Player/AnimationScripts/CatherisationPutClothUnder.cs
@@ -63,35 +63,6 @@ public class CatherisationPutClothUnder : CinematicAnimation
                     GameObject.Find("cloth_02").GetComponent<MeshRenderer>().enabled = true;
                 }
             }
-
-            // add new ghost positions for GauzeTrayFull
-            HandsInventory.GhostPosition posNormalGauze = new HandsInventory.GhostPosition();
-            posNormalGauze.objectName = "GauzeTrayWet";
-            posNormalGauze.position = new Vector3(9.438f, 1.1014f, 11.509f);
-            posNormalGauze.rotation = new Vector3(-90f, 0f, 0f);
-            posNormalGauze.id = 1;
-            inv.customGhostPositions.Add(posNormalGauze);
-
-            HandsInventory.GhostPosition posNewGauze = new HandsInventory.GhostPosition();
-            posNewGauze.objectName = "GauzeTrayWet";
-            posNewGauze.position = new Vector3(10.187f, 0.748f, 9.2253f);
-            posNewGauze.rotation = new Vector3(-90f, 0f, 87.576f);
-            posNewGauze.id = 2;
-            inv.customGhostPositions.Add(posNewGauze);
-
-            HandsInventory.GhostPosition posNormalTrash = new HandsInventory.GhostPosition();
-            posNormalTrash.objectName = "PlasticTrashbucket";
-            posNormalTrash.position = new Vector3(9.2011f, 1.0979f, 11.265f);
-            posNormalTrash.rotation = new Vector3(-90f, 0f, -90f);
-            posNormalTrash.id = 1;
-            inv.customGhostPositions.Add(posNormalTrash);
-
-            HandsInventory.GhostPosition posNewTrash = new HandsInventory.GhostPosition();
-            posNewTrash.objectName = "PlasticTrashbucket";
-            posNewTrash.position = new Vector3(9.9253f, 0.742f, 9.1915f);
-            posNewTrash.rotation = new Vector3(-90f, 0f, 90f);
-            posNewTrash.id = 2;
-            inv.customGhostPositions.Add(posNewTrash);
         }
     }
 
