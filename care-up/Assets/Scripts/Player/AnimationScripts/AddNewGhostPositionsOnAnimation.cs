adb28d81768e9db52d4cd3cf3d2ac1411a9e2ee8
Acvarium <vshmorgun@yandex.ru>
2018-11-08 15:24:52 +0200

    Corrected script, changed Ghost positions

--- care-up/Assets/Scripts/Player/AnimationScripts/AddNewGhostPositionsOnAnimation.cs
+++ care-up/Assets/Scripts/Player/AnimationScripts/AddNewGhostPositionsOnAnimation.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class AddNewGhostPositionsOnAnimation : StateMachineBehaviour
 {
-    public List<HandsInventory.GhostPosition> list = new List<HandsInventory.GhostPosition>();
-
-	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    [System.Serializable]
+    public struct GhostPositionData
     {
+        public string objectName;
+        public string TargetName;
+        public int id;
+    };
+
+    private List<HandsInventory.GhostPosition> list = new List<HandsInventory.GhostPosition>();
+    public List<GhostPositionData> dataList = new List<GhostPositionData>();
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        foreach (GhostPositionData d in dataList)
+        {
+            Transform targetTransform = GameObject.Find(d.TargetName).transform;
+
+            Vector3 pos = targetTransform.position;
+            Vector3 rot = targetTransform.rotation.eulerAngles;
+            HandsInventory.GhostPosition listItem = new HandsInventory.GhostPosition();
+            listItem.id = d.id;
+            listItem.position = pos;
+            listItem.rotation = rot;
+            listItem.objectName = d.objectName;
+            list.Add(listItem);
+        }
+
         HandsInventory inv = GameObject.FindObjectOfType<HandsInventory>();
         
         foreach (HandsInventory.GhostPosition i in list)
