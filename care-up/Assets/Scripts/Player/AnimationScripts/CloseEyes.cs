9e6213b169a106243618f5ecaf129a5729dcaa31
Acvarium <vshmorgun@yandex.ru>
2018-11-20 18:29:01 +0200

    Close and open eyes action

---
+++ care-up/Assets/Scripts/Player/AnimationScripts/CloseEyes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseEyes : StateMachineBehaviour {
+    public bool toClose = true;
+    public int actionFrame = 0;
+    protected float frame;
+    protected float prevFrame;
+
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+    
+
+        if (animator.speed != 0)
+        {
+            if (PlayerAnimationManager.CompareFrames(frame, prevFrame, actionFrame))
+            {
+                if (GameObject.FindObjectOfType<PlayerAnimationManager>() != null)
+                {
+                    Animator anim = GameObject.FindObjectOfType<PlayerAnimationManager>().GetComponent<Animator>();
+                    if (toClose)
+                    {
+                        anim.SetTrigger("close_eyes");
+                    }
+                    else
+                    {
+                        anim.SetTrigger("open_eyes");
+                    }
+                }
+
+            }
+            prevFrame = frame;
+            frame += Time.deltaTime;
+        }
+
+    }
+
+
+
+
+}
