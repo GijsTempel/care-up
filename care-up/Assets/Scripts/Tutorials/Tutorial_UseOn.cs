86caa9c703e2e4749f16944ae653190ee97503b4
DESKTOP-CTA26N5\Dani Steunebrink <danisteunebrink@live.nl>
2018-11-22 14:57:09 +0100

    tutorial UseOn pop up sounds dani steunebrink

--- care-up/Assets/Scripts/Tutorials/Tutorial_UseOn.cs
+++ care-up/Assets/Scripts/Tutorials/Tutorial_UseOn.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Tutorial_UseOn : TutorialManager {
 
+    public AudioClip Popup;
+    AudioSource audioSource;
+
     public enum TutorialStep
     {
         First,
@@ -25,9 +28,13 @@ public class Tutorial_UseOn : TutorialManager {
 
         if (!Paused())
         {
-            switch(currentStep)
+
+            audioSource = GetComponent<AudioSource> ();
+
+            switch (currentStep)
             {
                 case TutorialStep.First:
+                    audioSource.PlayOneShot (Popup, 0.1F);
                     currentStep = TutorialStep.Welcome;
                     //hintsBox.anchoredPosition = new Vector2(-14.8f, 9.9f);
                     //hintsBox.sizeDelta = new Vector2(788f, 524.9f);
@@ -44,9 +51,10 @@ public class Tutorial_UseOn : TutorialManager {
                 case TutorialStep.Welcome:
                     if (nextButtonClicked)
                     {
-						//hintsBox.anchoredPosition = new Vector2(165f, -265.64f);
-						//hintsBox.sizeDelta = new Vector2(472.5f, 298.9f);
-						hintsN.ResetSize();
+                        audioSource.PlayOneShot (Popup, 0.1F);
+                        //hintsBox.anchoredPosition = new Vector2(165f, -265.64f);
+                        //hintsBox.sizeDelta = new Vector2(472.5f, 298.9f);
+                        hintsN.ResetSize();
 						hintsN.LockTo("WorkField", new Vector3(0.00f, 0.51f, -0.73f));
                         currentStep = TutorialStep.MoveTo;
                         UItext.text = "Laten we beginnen door te bewegen naar het werkveld. Doe dit door te klikken op het werkveld.";
@@ -57,6 +65,7 @@ public class Tutorial_UseOn : TutorialManager {
                 case TutorialStep.MoveTo:
                     if (player.tutorial_movedTo)
                     {
+                        audioSource.PlayOneShot (Popup, 0.1F);
                         player.tutorial_movedTo = false;
 
                         currentStep = TutorialStep.PickNeedle;
@@ -76,6 +85,7 @@ public class Tutorial_UseOn : TutorialManager {
                 case TutorialStep.PickNeedle:
                     if (handsInventory.tutorial_pickedLeft)
                     {
+                        audioSource.PlayOneShot (Popup, 0.1F);
                         handsInventory.tutorial_pickedLeft = false;
                         itemToPick = "";
 
@@ -94,6 +104,7 @@ public class Tutorial_UseOn : TutorialManager {
                 case TutorialStep.OpenControls:
                     if (player.tutorial_itemControls)
                     {
+                        audioSource.PlayOneShot (Popup, 0.1F);
                         player.tutorial_itemControls = false;
 
                         currentStep = TutorialStep.ClickUseOn;
@@ -108,6 +119,7 @@ public class Tutorial_UseOn : TutorialManager {
                 case TutorialStep.ClickUseOn:
                     if (player.tutorial_UseOnControl)
                     {
+                        audioSource.PlayOneShot (Popup, 0.1F);
                         player.tutorial_UseOnControl = false;
                         
                         currentStep = TutorialStep.UseOn;
@@ -122,6 +134,7 @@ public class Tutorial_UseOn : TutorialManager {
                 case TutorialStep.UseOn:
                     if (handsInventory.tutorial_itemUsedOn)
                     {
+                        audioSource.PlayOneShot (Popup, 0.1F);
                         handsInventory.tutorial_itemUsedOn = false;
                         currentStep = TutorialStep.Done;
 						hintsN.LockTo("SceneLoader 1", new Vector3(363.61f, -22.40f, 0.00f));
