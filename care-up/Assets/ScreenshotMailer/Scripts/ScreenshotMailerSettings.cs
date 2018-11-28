a9e585d5bdeff026a27ba97709d0c8845835c77e
TripleMotion Media <gtempel@triplemotion.nl>
2018-11-28 15:39:32 +0100

    Deleted screenshot mailer

--- care-up/Assets/ScreenshotMailer/Scripts/ScreenshotMailerSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ScreenshotMailerSettings : ScriptableObject {
-
-    public string EmailAddress = "info@careup.nl";
-    public int SuperSize = 1;
-}
