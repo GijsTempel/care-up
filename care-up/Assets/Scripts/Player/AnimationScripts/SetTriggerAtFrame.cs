fc56b534b2e259cf38ff5a64b67ca12b3302c464
Acvarium <vshmorgun@yandex.ru>
2018-12-04 17:54:15 +0200

    Work on implementation

--- care-up/Assets/Scripts/Player/AnimationScripts/SetTriggerAtFrame.cs
+++ care-up/Assets/Scripts/Player/AnimationScripts/SetTriggerAtFrame.cs
@@ -7,24 +7,46 @@ public class SetTriggerAtFrame : StateMachineBehaviour
     private float currentFrame;
     private float prevFrame;
 
-    public int frame;
+    public int actionFrame;
     public string trigger;
+    public string ObjectName = "";
 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         currentFrame = 0f;
         prevFrame = 0f;
+        if (actionFrame == 0)
+        {
+            set_trigger(animator);
+        }
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    void set_trigger(Animator animator)
+    {
+        if (ObjectName == "")
+        {
+            animator.SetTrigger(trigger);
+        }
+        else
+        {
+            if (GameObject.Find(ObjectName) != null)
+            {
+                if (GameObject.Find(ObjectName).GetComponent<Animator>() != null)
+                {
+                    GameObject.Find(ObjectName).GetComponent<Animator>().SetTrigger(trigger);
+                }
+            }
+        }
+    }
+
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (animator.speed != 0)
         {
-            if (PlayerAnimationManager.CompareFrames(currentFrame, prevFrame, frame))
+            if (PlayerAnimationManager.CompareFrames(currentFrame, prevFrame, actionFrame))
             {
-                animator.SetTrigger(trigger);
+                set_trigger(animator);
             }
 
             prevFrame = currentFrame;
@@ -32,18 +54,4 @@ public class SetTriggerAtFrame : StateMachineBehaviour
         }
     }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
-    //}
 }
