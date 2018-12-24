5c4fdd74da65e0264eca3f45f65987edf81a02ec
Goizz <gijstempel@msn.com>
2018-12-24 14:13:09 +0100

    UI changes

--- care-up/Assets/Scripts/Menu/ScoreLine.cs
+++ care-up/Assets/Scripts/Menu/ScoreLine.cs
@@ -9,7 +9,7 @@ public class ScoreLine : MonoBehaviour {
     public void SetScoreLine(string _name, string _score, int i)
     {
         transform.Find("name").GetComponent<Text>().text = _name;
-        transform.Find("time/Text").GetComponent<Text>().text = "";
+        //transform.Find("time/Text").GetComponent<Text>().text = "";
         transform.Find("score/Text").GetComponent<Text>().text = _score;
         if (i < 3)
             crown.SetActive(_name != "");
