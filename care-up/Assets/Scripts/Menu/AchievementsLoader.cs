822b045352cf2066b879d22b625cc1d752a1b7e9
Dianash <ddiana.shnaider@gmail.com>
2019-03-14 19:57:28 +0200

    Created loader for Achievements (prestaties)

---
+++ care-up/Assets/Scripts/Menu/AchievementsLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AchievementsLoader : MonoBehaviour
+{
+    public GameObject accountAchievementsPanel;
+    public GameObject loadingIcon;
+    public GameObject backButton;
+    public GameObject image;
+    public GameObject title;
+    public GameObject separator;
+}
