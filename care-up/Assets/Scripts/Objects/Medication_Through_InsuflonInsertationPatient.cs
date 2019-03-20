e0866b7fe66a9d600352f9cd8bc619a46ce24a0a
PeterHarmatiy <harmatiy@gmail.com>
2019-03-20 11:50:50 +0200

    Created and added patien script and dialog xml to scene

---
+++ care-up/Assets/Scripts/Objects/Medication_Through_InsuflonInsertationPatient.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medication_Through_InsuflonInsertationPatient : PersonObject {
+
+    private Animator PatientAnimator;
+
+    private Animator PlayerAnimator;
+
+    public Vector3 playerPosition;
+    public Vector3 playerRotation;
+    public Transform playerPositionTarget;
+
+    protected override void Start()
+    {
+        base.Start();
+        PlayerAnimator = GameObject.FindObjectOfType<PlayerAnimationManager>().GetComponent<Animator>();
+        PatientAnimator = GetComponent<Animator>();
+    }
+
+    public override void Talk(string topic = "")
+    {
+        if (ViewModeActive() || topic == "CM_Leave" || topic == "")
+            return;
+
+        if (actionManager.CompareTopic(topic))
+        {
+            switch (topic)
+            {
+                case "show injection spot":
+                    PlayerAnimator.SetTrigger("BlanketEmpty");
+                    PlayerAnimator.SetTrigger("S BlanketEmpty");
+                    PlayerAnimator.SetTrigger("TakeOffBlanket");
+                    break;
+                default:
+                    break;
+            }
+
+            NextDialogue();
+        }
+
+        actionManager.OnTalkAction(topic);
+    }
+}
