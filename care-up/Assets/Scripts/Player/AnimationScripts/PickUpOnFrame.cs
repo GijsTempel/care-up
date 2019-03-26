e1be0b7f10852f78ab13f41cb3db9d6573b01f43
Dianash <ddiana.shnaider@gmail.com>
2019-03-26 15:08:40 +0200

    Implemented bladder insertion animation

---
+++ care-up/Assets/Scripts/Player/AnimationScripts/PickUpOnFrame.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class PickUpOnFrame : StateMachineBehaviour {
+
+    public int actionFrame;
+    public string objectName;
+    public PlayerAnimationManager.Hand hand;
+
+    protected float frame = 0f;
+    protected float prevFrame;
+
+    HandsInventory inventory;
+    GameObject obj = null;
+
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+
+       
+        inventory = GameObject.FindObjectOfType<HandsInventory>();
+        if(actionFrame == 0)
+        {
+            AddObject();
+        }
+    }    
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Debug.Log(frame);
+        if (PlayerAnimationManager.CompareFrames(frame, prevFrame, actionFrame))
+        {
+            AddObject();
+        }
+
+        if (animator.speed != 0)
+        {
+            prevFrame = frame;
+            frame += Time.deltaTime;
+        }
+    }
+
+    public void AddObject()
+    {
+        if (GameObject.Find(objectName) != null)
+        {
+            obj = GameObject.Find(objectName);
+            inventory.ForcePickItem(obj, hand);
+            PlayerAnimationManager.SetHandItem(hand == PlayerAnimationManager.Hand.Left, obj);
+        }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {        
+        frame = 0f;
+        obj = null;
+    }
+}
