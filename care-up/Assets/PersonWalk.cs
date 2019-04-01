d0ab3af644f9537e465a95c915842e2400719803
PeterHarmatiy <harmatiy@gmail.com>
2019-04-01 16:51:08 +0300

    Walking motion with activation from animation

---
+++ care-up/Assets/PersonWalk.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonWalk : StateMachineBehaviour {
+    public string PersonName;
+    public string PointsHolderName;
+    GameObject person = null;
+    GameObject KeyPointsHolder = null;
+    public float walkingSpeed = -1;
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if (GameObject.Find(PersonName) != null)
+        {
+            if (GameObject.Find(PersonName).GetComponent<MoveToPoint>() != null)
+                person = GameObject.Find(PersonName);
+        }
+        if (GameObject.Find(PointsHolderName) != null)
+        {
+            if (GameObject.Find(PointsHolderName).GetComponent<ExtraObjectOptions>() != null)
+                KeyPointsHolder = GameObject.Find(PointsHolderName);
+        }
+
+        if (person != null )
+        {
+            if (KeyPointsHolder != null)
+                person.GetComponent<MoveToPoint>().SetKeyPoints(PointsHolderName);
+            else
+                person.GetComponent<MoveToPoint>().StartWalking();
+            if (walkingSpeed > 0f)
+            {
+                person.GetComponent<MoveToPoint>().speed = walkingSpeed;
+            }
+        }
+        
+    }
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
---
+++ care-up/Assets/Scripts/Objects/MoveToPoint.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPoint : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public List<GameObject> KyePoints;
+    int NextPointIndex = 0;
+    public bool toWalk = false;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (NextPointIndex < KyePoints.Count)
+            transform.LookAt(KyePoints[NextPointIndex].transform.position);
+        
+    } 
+    public void SetKeyPoints(string PointHolderName)
+    {
+        if (GameObject.Find(PointHolderName) != null)
+        {
+            GameObject PointHolder = GameObject.Find(PointHolderName);
+            if (PointHolder.GetComponent<ExtraObjectOptions>() != null)
+            {
+                KyePoints.Clear();
+                foreach(GameObject g in PointHolder.GetComponent<ExtraObjectOptions>().hidenObjects)
+                {
+                    KyePoints.Add(g);
+                }
+                NextPointIndex = 0;
+                toWalk = true;
+            }
+        }
+    }
+
+    public void StartWalking()
+    {
+        NextPointIndex = 0;
+        toWalk = true;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (toWalk)
+        {
+            GameObject nextPoint = null;
+            if (NextPointIndex < KyePoints.Count)
+                nextPoint = KyePoints[NextPointIndex];
+            else
+            {
+                toWalk = false;
+            }
+            float step = speed * Time.deltaTime;
+
+            if (nextPoint != null)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, nextPoint.transform.position, step);
+
+                if (Vector3.Distance(transform.position, nextPoint.transform.position) < 0.001f)
+                {
+
+                    transform.position = nextPoint.transform.position;
+                    NextPointIndex++;
+                    transform.LookAt(KyePoints[NextPointIndex].transform.position);
+                }
+            }
+
+        }
+    }
+}
