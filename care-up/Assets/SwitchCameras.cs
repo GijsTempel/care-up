6691358270b7d6547194228499488eec2d0213ad
Acvarium <vshmorgun@yandex.ru>
2019-04-01 16:14:36 +0300

    Swtich camera

--- care-up/Assets/SwitchCameras.cs
+++ care-up/Assets/SwitchCameras.cs
@@ -9,38 +9,54 @@ public class SwitchCameras : StateMachineBehaviour {
     protected float frame;
     protected float prevFrame;
     private Camera playerCamera = null;
-    
+    private Camera switchCamera = null;
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        foreach (Camera c in GameObject.FindObjectsOfType<Camera>())
+        {
+            if (c.transform.parent != null)
+            {
+                if (c.transform.parent.name == "Head")
+                    playerCamera = c;
+            }
+            if (c.name == cameraName)
+                switchCamera = c;
+        }
+        if (backFrame > switchFrame && switchFrame == 0)
+        {
+            switchCam(false);
+        }
+    }
+
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+    
         if (animator.speed != 0)
         {
-            if (PlayerAnimationManager.CompareFrames(frame, prevFrame, switchFrame))
+            if (switchCamera != null && playerCamera != null && backFrame > switchFrame)
             {
-                foreach (Camera unit in Camera.allCameras)
-                {
-                    if (unit.enabled)
-                        playerCamera = unit;
-                    if (unit.name == cameraName)
-                        unit.enabled = true;
-                    else
-                        unit.enabled = false;
+                
+                if (PlayerAnimationManager.CompareFrames(frame, prevFrame, switchFrame))
+                    switchCam(false);
 
-                }
-            }
+                if (PlayerAnimationManager.CompareFrames(frame, prevFrame, backFrame) && playerCamera != null)
+                    switchCam(true);
 
-            if (PlayerAnimationManager.CompareFrames(frame, prevFrame, switchFrame) && playerCamera != null)
-            {
-                foreach (Camera unit in Camera.allCameras)
-                {
-                    unit.enabled = false;
-                   
-
-                }
-                playerCamera.enabled = true;
             }
             prevFrame = frame;
             frame += Time.deltaTime;
-        }       
+        }
+    }
+
+    void switchCam(bool toMain)
+    {
+        if (switchCamera != null && playerCamera != null)
+        {
+            switchCamera.enabled = !toMain;
+            playerCamera.enabled = toMain;
+        }
     }
 
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
