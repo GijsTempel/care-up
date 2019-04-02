17e3e3fb06ca56bf8f74dbf8c7886c69ec144305
Acvarium <vshmorgun@yandex.ru>
2019-04-02 18:52:04 +0300

    More s**t

--- care-up/Assets/Scripts/Menu/ItemControlsUI.cs
+++ care-up/Assets/Scripts/Menu/ItemControlsUI.cs
@@ -495,33 +495,28 @@ public class ItemControlsUI : MonoBehaviour {
 
         if (initedObject == handsInventory.LeftHandObject)
         {
-            if (handsInventory.LeftHandObject.name == "BloodGlucoseMeter")
-            {
-                GameObject.FindObjectOfType<PlayerAnimationManager>().GetComponent<Animator>().SetTrigger("turnon_left");
-            }
-            else
-            {
-                handsInventory.LeftHandObject.GetComponent<PickableObject>().Use(true, true);
+            //if (handsInventory.LeftHandObject.name == "BloodGlucoseMeter")
+            //{
+            //    GameObject.FindObjectOfType<PlayerAnimationManager>().GetComponent<Animator>().SetTrigger("turnon_left");
+            //}
+            
+            handsInventory.LeftHandObject.GetComponent<PickableObject>().Use(true, true);
 
-                if (tutorialUseOn != null)
-                {
-                    handsInventory.LeftHandObject.GetComponent<PickableObject>().tutorial_usedOn = true;
-                }
+            if (tutorialUseOn != null)
+            {
+                handsInventory.LeftHandObject.GetComponent<PickableObject>().tutorial_usedOn = true;
+                
             }
         }
         else
         {
-            if (handsInventory.RightHandObject.name == "BloodGlucoseMeter")
+            //if (handsInventory.RightHandObject.name == "BloodGlucoseMeter")
+            //{
+            //    GameObject.FindObjectOfType<PlayerAnimationManager>().GetComponent<Animator>().SetTrigger("turnon_right");
+            handsInventory.RightHandObject.GetComponent<PickableObject>().Use(false, true);
+            if (tutorialUseOn != null)
             {
-                GameObject.FindObjectOfType<PlayerAnimationManager>().GetComponent<Animator>().SetTrigger("turnon_right");
-            }
-            else
-            {
-                handsInventory.RightHandObject.GetComponent<PickableObject>().Use(false, true);
-                if (tutorialUseOn != null)
-                {
-                    handsInventory.RightHandObject.GetComponent<PickableObject>().tutorial_usedOn = true;
-                }
+                handsInventory.RightHandObject.GetComponent<PickableObject>().tutorial_usedOn = true;
             }
         }
 
