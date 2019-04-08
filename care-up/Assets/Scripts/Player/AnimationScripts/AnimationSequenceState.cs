1519b7080d5ee5b5017968192f203b9d1e21b99b
Acvarium <vshmorgun@yandex.ru>
2019-04-08 14:52:07 +0300

    Few mode fixed

--- care-up/Assets/Scripts/Player/AnimationScripts/AnimationSequenceState.cs
+++ care-up/Assets/Scripts/Player/AnimationScripts/AnimationSequenceState.cs
@@ -64,6 +64,7 @@ public class AnimationSequenceState : StateMachineBehaviour {
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
         if (TuggleFromCinematic)
         {
             CameraMode cameraMode = GameObject.Find("GameLogic").GetComponent<CameraMode>();
--- care-up/Assets/Scripts/Player/AnimationScripts/Cinematic.cs
+++ care-up/Assets/Scripts/Player/AnimationScripts/Cinematic.cs
@@ -7,6 +7,7 @@ public class Cinematic : StateMachineBehaviour
     public string target;
     public bool resetCamera = false;
     protected CameraMode mode;
+    public bool ExitCinematicAtEnd = false;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -21,6 +22,10 @@ public class Cinematic : StateMachineBehaviour
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         mode.animationEnded = true;
+        if (ExitCinematicAtEnd)
+        {
+            mode.ToggleCameraMode(CameraMode.Mode.Free);
+        }
     }
 
    
