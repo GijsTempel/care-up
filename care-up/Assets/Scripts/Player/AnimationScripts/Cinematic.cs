d0af28ad2059abd44f9e4dfcda15d73dd7b678ca
Acvarium <vshmorgun@yandex.ru>
2019-04-22 14:01:02 +0300

    Fixed broke cinematic

--- care-up/Assets/Scripts/Player/AnimationScripts/Cinematic.cs
+++ care-up/Assets/Scripts/Player/AnimationScripts/Cinematic.cs
@@ -8,23 +8,20 @@ public class Cinematic : StateMachineBehaviour
     public bool resetCamera = false;
     protected CameraMode mode;
 
-    protected float prevFrame;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        mode = GameObject.FindObjectOfType<CameraMode>();
-        if (GameObject.Find(target) != null)
-        {
-            if (GameObject.Find(target).transform.Find("CinematicTarget") != null)
-            {
-                float dist = Vector3.Distance(GameObject.Find(target).transform.Find("CinematicTarget").transform.position,
-                    animator.transform.position);
-            }
-        }
-       
+        mode = GameObject.Find("GameLogic").GetComponent<CameraMode>();
+        mode.SetCinematicMode(GameObject.Find(target).transform);
         if (resetCamera)
         {
             Camera.main.transform.localRotation = Quaternion.Euler(Vector3.zero);
         }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        mode.animationEnded = true;
+     
     }
 
    
