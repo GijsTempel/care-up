be0c72889f6e277586553e600beb2c394958ea90
Acvarium <vshmorgun@yandex.ru>
2019-04-22 12:39:45 +0300

    Removed unused code

--- care-up/Assets/Scripts/Player/AnimationScripts/Cinematic.cs
+++ care-up/Assets/Scripts/Player/AnimationScripts/Cinematic.cs
@@ -6,11 +6,7 @@ public class Cinematic : StateMachineBehaviour
 {
     public string target;
     public bool resetCamera = false;
-    public float maxDist = 1.8f;
     protected CameraMode mode;
-    bool teleport = false;
-    int teleportStartFrame;
-    protected float frame;
 
     protected float prevFrame;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -22,52 +18,13 @@ public class Cinematic : StateMachineBehaviour
             {
                 float dist = Vector3.Distance(GameObject.Find(target).transform.Find("CinematicTarget").transform.position,
                     animator.transform.position);
-                if (dist > maxDist)
-                {
-                    //teleport = true;
-                    //animator.SetTrigger("close_eyes");
-                    //teleportStartFrame = (int)(frame * 60) + 10;
-                }
             }
         }
-        if (!teleport)
-        {
-            mode.SetCinematicMode(GameObject.Find(target).transform);
-        }
+       
         if (resetCamera)
         {
             Camera.main.transform.localRotation = Quaternion.Euler(Vector3.zero);
         }
-    }
-
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        if (animator.speed != 0)
-        {
-            if (teleport)
-            {
-                if (PlayerAnimationManager.CompareFrames(frame, prevFrame, teleportStartFrame))
-                {
-                    mode.Teleport(GameObject.Find(target).transform);
-                }
-
-                if (PlayerAnimationManager.CompareFrames(frame, prevFrame, teleportStartFrame + 10))
-                { 
-                    animator.SetTrigger("open_eyes");
-                }
-            }
-            prevFrame = frame;
-            frame += Time.deltaTime;
-        }
-    }
-
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        if (!teleport)
-            mode.animationEnded = true;
-        teleport = false;
-        frame = 0;
-        prevFrame = 0;
     }
 
    
