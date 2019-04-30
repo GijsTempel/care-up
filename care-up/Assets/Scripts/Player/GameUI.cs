7ce79942f2c5dd5086102562f11de391ba691196
Acvarium <vshmorgun@yandex.ru>
2019-04-30 16:37:19 +0300

    Improved search for highlights

--- care-up/Assets/Scripts/Player/GameUI.cs
+++ care-up/Assets/Scripts/Player/GameUI.cs
@@ -450,8 +450,27 @@ public class GameUI : MonoBehaviour
                     {
                         HighlightObject h = AddHighlight(usableHL.transform, prefix, HighlightObject.type.NoChange, 2f + Random.Range(0f, 0.5f));
                         if (h != null)
+                        {
                             h.setGold(true);
-                        newHLObjects.Add(usableHL.name);
+                            newHLObjects.Add(usableHL.name);
+                        }
+                        
+
+                    }
+                    else
+                    {
+                        foreach (PickableObject p in GameObject.FindObjectsOfType<PickableObject>())
+                        {
+                            if (p.prefabInHands == objectToUse && p.prefabInHands != "")
+                            {
+                                HighlightObject h = AddHighlight(p.transform, prefix, HighlightObject.type.NoChange, 2f + Random.Range(0f, 0.5f));
+                                if (h != null)
+                                {
+                                    h.setGold(true);
+                                    newHLObjects.Add(usableHL.name);
+                                }
+                            }
+                        }
                     }
                 }
             }
