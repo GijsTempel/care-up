67997ec8586749fe9f5aad2e500167036498cec2
Alexander <olexandrzhytaryuk@gmail.com>
2019-07-31 20:27:51 +0300

    warning fixes

--- care-up/Assets/ResetArms.cs
+++ care-up/Assets/ResetArms.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ResetArms : StateMachineBehaviour
 {
     Transform arms;
-    Transform oldPosition;
+    Transform oldPosition = default(Transform);
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
