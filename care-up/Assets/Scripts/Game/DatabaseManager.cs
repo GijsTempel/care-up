5b38c8c9b023ff24adf635fa924412eff1ec7a6b
Dianash <ddiana.shnaider@gmail.com>
2019-07-31 13:16:33 +0300

    Returned main menu loading

--- care-up/Assets/Scripts/Game/DatabaseManager.cs
+++ care-up/Assets/Scripts/Game/DatabaseManager.cs
@@ -95,14 +95,14 @@ public class DatabaseManager : MonoBehaviour
              FetchField("AccountStats", "BIG_number") != ""))
         {
             WULogin.characterCreated = true;
-            if (FetchField("AccountStats", "TutorialCompleted") == "true")
-            {
+            //if (FetchField("AccountStats", "TutorialCompleted") == "true")
+            //{
                 bl_SceneLoaderUtils.GetLoader.LoadLevel("MainMenu");
-            }
-            else
-            {
-                bl_SceneLoaderUtils.GetLoader.LoadLevel("Scenes_Tutorial", "Scenes");
-            }
+            //}
+            //else
+            //{
+            //    bl_SceneLoaderUtils.GetLoader.LoadLevel("Scenes_Tutorial", "Scenes");
+            //}
         }
         else
         {
