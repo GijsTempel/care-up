69a5b86acc27fbfaab4d85a5e1ac4fd51a3d57c3
Vitalii Shmorgun <vshmorgun@yandex.ru>
2019-08-28 18:32:32 +0300

    Improved hide scripts

--- tools/hideAssets.py
+++ tools/hideAssets.py
@@ -2,12 +2,27 @@
 import os, sys
 base_folder = "../care-up/Assets/"
 with open("folders_to_hide.txt") as f:
-	folders_names = f.readlines()
-	for folder in folders_names:
-		folder_name = folder.replace("\n", "")
-		if os.path.exists(base_folder + folder_name):
-			os.rename(base_folder + folder_name,base_folder + "." + folder_name)
-			print("++ Hide " + folder_name);
+	for folder in f.readlines():
+		full_path = base_folder + folder.replace("\n", "")
+		object_name = full_path.split("/")[-1]
+		content_folder = full_path[0:len(full_path) - len(object_name)]
+		hide_path = content_folder + "." + object_name
+
+		a = full_path
+		b = hide_path
+		print_word = "++ Hide "
+
+		if(len(sys.argv) > 1):
+			if sys.argv[1] == 'u':
+				a = hide_path
+				b = full_path
+				print_word = "-- Unhidden "
+
+		if os.path.exists(a):
+			os.rename(a,b)
+			print(print_word + full_path);
+		if os.path.exists(a + ".meta"):
+			os.rename(a + ".meta",b + ".meta")
 		else:
-			print(folder_name + " not found")
+			print(" *  " + full_path + " not found")
 
--- tools/unhideAssets.py
+++ tools/unhideAssets.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python
 import os, sys
-base_folder = "../care-up/Assets/"
-with open("folders_to_hide.txt") as f:
-	folders_names = f.readlines()
-	for folder in folders_names:
-		folder_name = folder.replace("\n", "")
-		if os.path.exists(base_folder + "." + folder_name):
-			os.rename(base_folder + "." + folder_name,base_folder + folder_name)
-			print("++ Unhide " + folder_name);
-		else:
-			print(folder_name + " not found")
+os.system('python hideAssets.py u')
 
