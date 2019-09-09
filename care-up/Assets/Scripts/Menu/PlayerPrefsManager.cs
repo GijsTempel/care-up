79919bcdaab054102b244c4b7248d448cf31f5d7
Vitalii Shmorgun <vshmorgun@yandex.ru>
2019-09-09 17:51:12 +0300

    Fixed issue with no saving of mute state

--- care-up/Assets/Scripts/Menu/PlayerPrefsManager.cs
+++ care-up/Assets/Scripts/Menu/PlayerPrefsManager.cs
@@ -228,6 +228,12 @@ public class PlayerPrefsManager : MonoBehaviour
         set { PlayerPrefs.SetFloat("Volume", value); }
     }
 
+    public float ScroopPosition
+    {
+        get { return PlayerPrefs.HasKey("ScroopPosition") ? PlayerPrefs.GetFloat("ScroopPosition") : 0f; }
+        set { PlayerPrefs.SetFloat("ScroopPosition", value); }
+    }
+
     public void SetSceneActivated(string sceneName, bool value)
     {
         if (value) Debug.Log(sceneName + " activated");
