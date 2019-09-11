c09f59cf8c17c2cdd36007005b5106110d3d1ac5
Vitalii Shmorgun <vshmorgun@yandex.ru>
2019-09-11 18:21:53 +0300

    Carousel

---
+++ care-up/Assets/Scripts/Menu/CharacterCarousel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCarousel : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    public void Turn(int dir)
+    {
+        if (dir == 1)
+        {
+            GetComponent<Animator>().SetTrigger("next");
+        }
+        else if(dir == -1)
+        {
+            GetComponent<Animator>().SetTrigger("prev");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
