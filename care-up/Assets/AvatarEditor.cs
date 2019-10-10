65cbd56675e129c33fbb6daf31f4c42325208b8e
Vitalii Shmorgun <3dvits@gmail.com>
2019-10-10 12:18:56 +0300

    Hat selector

--- care-up/Assets/AvatarEditor.cs
+++ care-up/Assets/AvatarEditor.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 using CareUpAvatar;
 using UnityEngine.UI;
 
+
 public class AvatarEditor : MonoBehaviour
 {
+    public enum Selections
+    {
+        Hats,
+    };
+
     public PlayerAvatar MainAvatar;
     public InputField HeadInput;
     public InputField BodyInput;
     public InputField GenderInput;
     public InputField GlassesInput;
+    public InputField HatInput;
+
+    Selections currentSelection = new Selections();
+    List<string> Hats = new List<string>
+            { "None", "ball", "cowboy_hat", "cube", "cyl", "winter_hat" };
 
     public GameObject Tools;
     public GameObject Selector;
@@ -30,8 +41,30 @@ public class AvatarEditor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SelectorContent = Selector.transform.Find("Scroll View/Viewpor/Content");
+        SelectorContent = Selector.transform.Find("Scroll View/Viewport/Content");
+        print(SelectorContent.name);
         Invoke("Initialize", 0.1f);
+    }
+
+    public void BuildHatSelector()
+    {
+        currentSelection = Selections.Hats;
+        foreach (Transform child in SelectorContent) {
+            GameObject.Destroy(child.gameObject);
+        }
+        foreach (string h in Hats)
+        {
+            GameObject _Button = GameObject.Instantiate(Resources.Load<GameObject>("NecessaryPrefabs/UI/SelectorButton"), SelectorContent);
+            _Button.transform.Find("Text").GetComponent<Text>().text = h;
+            _Button.GetComponent<Button>().onClick.AddListener(() => SelectElement(h));
+            string category = "hat";
+            Sprite sprite = Resources.Load("Sprites/StoreItemPreview/" + category + "_" + h, typeof(Sprite)) as Sprite;
+            if (sprite != null)
+            {
+                _Button.transform.Find("Image").GetComponent<Image>().sprite = sprite;
+            }
+        }
+        ShowSelector(true);
     }
 
     public void ShowSelector(bool value)
@@ -44,6 +77,20 @@ public class AvatarEditor : MonoBehaviour
     {
         print(element);
         ShowSelector(false);
+        if (currentSelection == Selections.Hats)
+        {
+            if(element == "None")
+            {
+                MainAvatar.avatarData.hat = "";
+                HatInput.text = "";
+            }
+            else
+            {
+                MainAvatar.avatarData.hat = element;
+                HatInput.text = element;
+            }
+            MainAvatar.UpdateCharacter();
+        }
     }
 
     public void SetTab(int value)
