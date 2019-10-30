a769365d5cfb29b86f3959533d8d59f17c849f6a
PeterHarmatiy <harmatiy@gmail.com>
2019-10-30 18:36:00 +0200

    Worked on animation + dictionary

--- care-up/Assets/Scripts/Cheats/LocalizationManager.cs
+++ care-up/Assets/Scripts/Cheats/LocalizationManager.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using SimpleJSON;
 using UnityEngine;
 
-public class LocalizationManager {
+public class LocalizationManager
+{
     string[] dicts = new string[] {
         "TextData",
-        "Text_Ostomy_Care"
+        "Text_Ostomy_Care",
+        "TextData_AED"
     };
 
     public static LocalizationManager instance;
@@ -18,16 +18,18 @@ public class LocalizationManager {
 
     // Use this for initialization
 
-    public void LoadLocalizedText (string fileName) {
+    public void LoadLocalizedText(string fileName)
+    {
         gameLogic = GameObject.Find("GameLogic");
         if (localizedText == null)
-            localizedText = new Dictionary<string, string> ();
+            localizedText = new Dictionary<string, string>();
         TextAsset _data = (TextAsset)Resources.Load("Dictionaries/" + fileName);
         string jsonString = _data.text;
-        JSONNode data = JSON.Parse (jsonString);
-        foreach (string key in data.Keys) {
-            if (!localizedText.ContainsKey (key))
-                localizedText.Add (key, data[key]);
+        JSONNode data = JSON.Parse(jsonString);
+        foreach (string key in data.Keys)
+        {
+            if (!localizedText.ContainsKey(key))
+                localizedText.Add(key, data[key]);
         }
 
         isReady = true;
@@ -36,27 +38,33 @@ public class LocalizationManager {
     public void LoadAllDictionaries()
     {
         //Resources.Load("Dictionaries/");
-        foreach (string fileName in dicts) {
+        foreach (string fileName in dicts)
+        {
             LoadLocalizedText(fileName);
         }
     }
 
-    public string GetValueIfKey (string key) {
+    public string GetValueIfKey(string key)
+    {
         if (key == null)
             return "";
         if (key.Length == 0)
             return "";
         string result = key;
         bool debugMode = false;
-        if (gameLogic != null){
+        if (gameLogic != null)
+        {
             debugMode = gameLogic.GetComponent<ActionManager>().TextDebug;
         }
 
-        if (key[0] == '[') {
-            string value = GetLocalizedValue (key.Substring (1, key.Length - 2));
-            if (value != null){
+        if (key[0] == '[')
+        {
+            string value = GetLocalizedValue(key.Substring(1, key.Length - 2));
+            if (value != null)
+            {
                 result = value;
-                if (Application.isEditor && debugMode){
+                if (Application.isEditor && debugMode)
+                {
                     result = key + value;
                 }
             }
@@ -64,10 +72,14 @@ public class LocalizationManager {
         return result;
     }
 
-    public string GetLocalizedValue (string key) {
-        if (localizedText != null) {
-            if (localizedText.Keys.Count > 0) {
-                if (localizedText.ContainsKey (key)) {
+    public string GetLocalizedValue(string key)
+    {
+        if (localizedText != null)
+        {
+            if (localizedText.Keys.Count > 0)
+            {
+                if (localizedText.ContainsKey(key))
+                {
                     return localizedText[key];
                 }
             }
@@ -75,8 +87,8 @@ public class LocalizationManager {
         return null;
     }
 
-    public bool GetIsReady () {
+    public bool GetIsReady()
+    {
         return isReady;
     }
-
 }
