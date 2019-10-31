64abf1269dbcf12a7d85b501498219db099fc319
PeterHarmatiy <harmatiy@gmail.com>
2019-10-31 18:59:34 +0200

    Remade part of animation

--- care-up/Assets/Scripts/Player/GameUI.cs
+++ care-up/Assets/Scripts/Player/GameUI.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 using CareUp.Actions;
 using System.Linq;
 using AssetBundles;
+using UnityEngine.SceneManagement;
 
 
 
@@ -393,6 +394,8 @@ public class GameUI : MonoBehaviour
         {
             ActionManager.practiceMode = prefs.practiceMode;
         }
+
+        SetAEDLogic();
 
 #if !(UNITY_EDITOR || DEVELOPMENT_BUILD)
         if(GameObject.Find("ActionsPanel") != null)
@@ -1126,6 +1129,27 @@ public class GameUI : MonoBehaviour
             }
             float alpha = DetailedHintPanel.GetComponent<Image>().color.a;
             SetHintPanelAlpha(alpha);
+        }
+    }
+
+    private void SetAEDLogic()
+    {
+        if (SceneManager.GetActiveScene().name == "Scenes_AED")
+        {
+            Animator playerAnimator;
+            PlayerAnimationManager playerAnimationManager = GameObject.FindObjectOfType<PlayerAnimationManager>();
+
+            if (playerAnimationManager != null)
+            {
+                playerAnimator = playerAnimationManager.GetComponent<Animator>();
+
+                if (noTargetButton_right != null && playerAnimator != null)
+                {
+                    noTargetButton_right.SetActive(true);
+                    noTargetButton_right.gameObject.GetComponent<Button>().onClick.AddListener(() => playerAnimator.SetTrigger("Start_AED_SQ1"));
+                    noTargetButton_right.transform.GetChild(0).GetComponent<Text>().text = "Help de cliënt";
+                }
+            }
         }
     }
 }
