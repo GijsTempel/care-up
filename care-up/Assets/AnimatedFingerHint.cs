a96ccd215ba596d9ea5be47315732d4cbc8fd939
Vitalii Shmorgun <vshmorgun@yandex.ru>
2019-12-05 18:23:51 +0200

    Work on finger

--- care-up/Assets/AnimatedFingerHint.cs
+++ care-up/Assets/AnimatedFingerHint.cs
@@ -6,35 +6,83 @@ using UnityEngine.UI;
 
 public class AnimatedFingerHint : MonoBehaviour
 {
+    public bool toMove = false;
+    bool toWave = false;
+    bool shown = false;
     public Canvas myCanvas;
     public RectTransform FingerHolder;
+    public RectTransform TargetPointer;
+
     public Animator fingerAnimator;
     float moveSpeed = 500f;
     Vector3 moveTarget = new Vector3();
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveTarget = FingerHolder.position;
     }
-   
+    void ShowHand(bool toShow, bool showEffect = false, bool _toWave = false)
+    {
+        if (toShow)
+        {
+            if (!FingerHolder.gameObject.activeSelf)
+            {
+                FingerHolder.gameObject.SetActive(true);
+                toMove = true;
+            }
+            if (showEffect)
+            {
+                toMove = false;
+                fingerAnimator.SetTrigger("show");
+                if (_toWave)
+                    fingerAnimator.SetTrigger("hi");
+            }
+        }
+        else
+        {
+            FingerHolder.gameObject.SetActive(false);
+            toMove = false;
+        }
+
+    }
+
+    public void startMove()
+    {
+
+    }
+
     void Update()
     {
-        FingerHolder.position = Vector3.MoveTowards(FingerHolder.position, moveTarget, Time.deltaTime * moveSpeed);
+        if (toMove)
+        {
+            FingerHolder.position = Vector3.MoveTowards(FingerHolder.position, moveTarget, Time.deltaTime * moveSpeed);
+        }
         if (Input.GetMouseButtonDown(0))
         {
             fingerAnimator.SetTrigger("fly");
+            toMove = true;
             Vector2 pos;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out pos);
             moveTarget = myCanvas.transform.TransformPoint(pos);
+            TargetPointer.position = moveTarget;
             float dist = Vector3.Distance(FingerHolder.position, moveTarget);
             print(dist);
             moveSpeed = dist * 1f;
         }
 
         if (Input.GetMouseButtonDown(1))
+        {
+            ShowHand(!FingerHolder.gameObject.activeSelf, true, toWave);
             Debug.Log("Pressed secondary button.");
+        }
 
         if (Input.GetMouseButtonDown(2))
-            Debug.Log("Pressed middle click.");
+        {
+            fingerAnimator.SetTrigger("hi");
+
+            //Debug.Log("Pressed middle click.");
+            //toWave = !toWave;
+            //print(toWave);
+        }
     }
 }
