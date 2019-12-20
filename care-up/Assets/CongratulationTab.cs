6e018f0029f0b4d25c0f095b6a2cef5efc2d3ff4
Vitalii Shmorgun <vshmorgun@yandex.ru>
2019-12-20 16:53:44 +0200

    Work on implementing of new pop-up

---
+++ care-up/Assets/CongratulationTab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratulationTab : MonoBehaviour
+{
+    public GameObject diamantEffect;
+
+    public void ShowDiamantEffect(bool value)
+    {
+        diamantEffect.SetActive(value);
+    }
+}
