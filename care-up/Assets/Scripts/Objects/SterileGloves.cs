9ec26fdd2f4d872987e377d7cddbcf5860d40a5e
PeterHarmatiy <harmatiy@gmail.com>
2020-02-18 18:41:54 +0200

    Fixed problem with puting on sterile gloves

---
+++ care-up/Assets/Scripts/Objects/SterileGloves.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SterileGloves : UsableObject
+{
+    public bool oneTimeUse = false;
+
+    public override void Use()
+    {
+        if (!ViewModeActive())
+        {
+            if (actionManager.CompareUseObject(name))
+            {
+                PlayerAnimationManager.PlayAnimation("Use sterileGloves");
+                tutorial_used = true;
+
+                if (oneTimeUse)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+
+            actionManager.OnUseAction(gameObject.name);
+            Reset();
+        }
+    }
+}
