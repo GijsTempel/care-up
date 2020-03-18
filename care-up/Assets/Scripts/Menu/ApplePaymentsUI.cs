aecd0a53734ead76a08c36f2fadc45daa7cf5585
Dianash <ddiana.shnaider@gmail.com>
2020-03-18 15:02:57 +0200

    Added apple only text component to WUSerialScreen

---
+++ care-up/Assets/Scripts/Menu/ApplePaymentsUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ApplePaymentsUI : MonoBehaviour
+{
+    [TextArea]
+    public string AppleOnlyText;
+
+#if UNITY_IOS
+    private void Start()
+    {
+        gameObject.GetComponent<Text>().text = AppleOnlyText;
+    }
+#endif
+}
