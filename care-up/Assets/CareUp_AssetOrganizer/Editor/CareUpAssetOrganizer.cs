3658e1c8cadad83472092abf7563c5719081f512
Vitalii Shmorgun <vshmorgun@yandex.ru>
2020-11-30 14:35:55 +0200

    Removed extra progress bar action

--- care-up/Assets/CareUp_AssetOrganizer/Editor/CareUpAssetOrganizer.cs
+++ care-up/Assets/CareUp_AssetOrganizer/Editor/CareUpAssetOrganizer.cs
@@ -5,45 +5,6 @@ using UnityEditor;
 using System.IO;
 using UnityEditor.AddressableAssets;
 
-
-public class EditorUtilityDisplayProgressBar : EditorWindow
-{
-    public float secs = 10f;
-    public float startVal = 0f;
-    public float progress = 0f;
-    [MenuItem("Tools/Progress Bar Usage")]
-    static void Init()
-    {
-        UnityEditor.EditorWindow window = GetWindow(typeof(EditorUtilityDisplayProgressBar));
-        window.Show();
-    }
-
-    void OnGUI()
-    {
-        secs = EditorGUILayout.FloatField("Time to wait:", secs);
-        if (GUILayout.Button("Display bar"))
-        {
-            if (secs < 1)
-            {
-                Debug.LogError("Seconds should be bigger than 1");
-                return;
-            }
-            startVal = (float)EditorApplication.timeSinceStartup;
-        }
-        if (progress < secs)
-            EditorUtility.DisplayProgressBar("Simple Progress Bar", "Shows a progress bar for the given seconds", progress / secs);
-        else
-            EditorUtility.ClearProgressBar();
-        progress = (float)(EditorApplication.timeSinceStartup - startVal);
-    }
-
-    void OnInspectorUpdate()
-    {
-        Repaint();
-    }
-}
-
-
 public class CareUpAssetOrganizer : EditorWindow
 {
     public static int itemsToProsess = 1;
