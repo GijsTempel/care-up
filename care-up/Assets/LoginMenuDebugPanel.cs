843760daf3f3608e205d17727c290064fe5bfc9f
Acvarium <3dvits@gmail.com>
2021-05-11 17:04:33 +0300

    Added code to get params from url

--- care-up/Assets/LoginMenuDebugPanel.cs
+++ care-up/Assets/LoginMenuDebugPanel.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Runtime.InteropServices;
 
 public class LoginMenuDebugPanel : MonoBehaviour
 {
+
+    [DllImport("__Internal")]
+    private static extern string GetStringParams();
+    public Text TestOutputComponentText;
     bool _visible = false;
     public void Toggle()
     {
@@ -14,5 +18,7 @@ public class LoginMenuDebugPanel : MonoBehaviour
             GetComponent<Animator>().SetTrigger("show");
         else
             GetComponent<Animator>().SetTrigger("hide");
+
+        TestOutputComponentText.text = "_ " + GetStringParams();
     }
 }
