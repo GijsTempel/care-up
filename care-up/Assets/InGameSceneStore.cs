46332c82c5c8cfbc97f202cce79a670e27120f65
Acvarium <3dvits@gmail.com>
2021-05-17 18:44:40 +0300

    Worked on the scene store

--- care-up/Assets/InGameSceneStore.cs
+++ care-up/Assets/InGameSceneStore.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+public class SSPackageData
+{
+    public string title;
+    public string SKU;
+    public List<string> scenes;
+    public float price;
+    public SSPackageData(string _title, string _SKU, float _price)
+    {
+        title = _title;
+        SKU = _SKU;
+        price = _price;
+    }
+}
 public class InGameSceneStore : MonoBehaviour
 {
     public List<GameObject> tabs;
     public List<GameObject> tabsButtons;
+    public GameObject packageStoreContent;
+    public GameObject sceneStoreContent;
+
     Sprite openTabImage = null;
     Sprite closeTabImage = null;
-
+    Object SSPackageItemPrefab = Resources.Load<GameObject>("NecessaryPrefabs/UI/SSPackageItem");
     int currentTab = 0;
     public void SwitchTab(int newTab)
     {
@@ -28,16 +43,41 @@ public class InGameSceneStore : MonoBehaviour
         }
         currentTab = newTab;
     }
+    void LoadStoreData()
+    {
+        if (SSPackageItemPrefab == null)
+            SSPackageItemPrefab = Resources.Load<GameObject>("NecessaryPrefabs/UI/SSPackageItem");
+        //test data
+        List<SSPackageData> SSPData = new List<SSPackageData>();
 
+        SSPData.Add(new SSPackageData("Bloedglucose pakket", "aaa", 123f));
+        SSPData.Add(new SSPackageData("Handhygiëne pakket", "aaa", 123f));
+        SSPData.Add(new SSPackageData("Injecteren pakket", "aaa", 123f));
+        SSPData.Add(new SSPackageData("Katheteriseren pakket", "aaa", 123f));
+
+        string[] _sstrings = new string[] { 
+            "Injecteren intramusculair (loodrechttechniek)",
+            "Injecteren subcutaan (huidplooitechniek)",
+            "Injecteren subcutaan (loodrechttechniek)",
+            "Subcutaan injecteren met insulinepen",
+            "Voorbereiden medicijn injecteren ampul",
+            "Voorbereiden medicijn injecteren flacon",
+            "Voorbereiden medicijn injecteren oplossing" };
+
+        List<string> __scenes = new List<string>();
+        __scenes.AddRange(_sstrings);
+
+        for (int i = 0; i < SSPData.Count; i++)
+        {
+            GameObject packageButton = Instantiate(SSPackageItemPrefab, packageStoreContent.transform) as GameObject;
+            packageButton.GetComponent<SSPackageItem>().Setup(SSPData[i].title, __scenes, "", SSPData[i].price);
+        }
+    }
     void Start()
     {
         openTabImage = tabsButtons[0].GetComponent<Image>().sprite;
         closeTabImage = tabsButtons[1].GetComponent<Image>().sprite;
-
+        LoadStoreData();
     }
 
-    void Update()
-    {
-        
-    }
 }
---
+++ care-up/Assets/SSPackageItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SSPackageItem : MonoBehaviour
+{
+    public Text titleText;
+    public Text priceText;
+    public float price;
+    public string packageSKU = "";
+    public void Setup(string _title, List<string> _scenes, string _packageSKU, float _price)
+    {
+        price = _price;
+        priceText.text = "€" + price.ToString("F2");
+        Object SceneItemPrefab = Resources.Load<GameObject>("NecessaryPrefabs/UI/SSPackageItemScene");
+        titleText.text = _title;
+        packageSKU = _packageSKU;
+        Transform sceneSlot = transform.Find("SSNames");
+        GameObject spacer1 = GameObject.Instantiate(Resources.Load<GameObject>("NecessaryPrefabs/UI/SSPackageSpacer"), sceneSlot);
+        Vector2 spacer1Size = spacer1.GetComponent<RectTransform>().sizeDelta;
+        spacer1Size.y = 10f;
+        spacer1.GetComponent<RectTransform>().sizeDelta = spacer1Size;
+
+        if (_scenes != null)
+        {
+            for (int i = 0; i < _scenes.Count; i++)
+            {
+                GameObject SceneItemObject = Instantiate(SceneItemPrefab, sceneSlot) as GameObject;
+                SceneItemObject.transform.Find("Text").GetComponent<Text>().text = "• " + _scenes[i];
+            }
+            GameObject.Instantiate(Resources.Load<GameObject>("NecessaryPrefabs/UI/SSPackageSpacer"), sceneSlot);
+        }
+    }
+
+    public void ButtonPressed()
+    {
+
+    }
+}
