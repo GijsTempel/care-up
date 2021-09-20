5e05b4ff1f1a6106bead71f285a40eca70b4c0f7
Acvarium <3dvits@gmail.com>
2021-09-20 14:10:36 +0300

    Hide GetStringParams from non WebGL builds

--- care-up/Assets/LoginMenuDebugPanel.cs
+++ care-up/Assets/LoginMenuDebugPanel.cs
@@ -7,8 +7,6 @@ using System.Runtime.InteropServices;
 public class LoginMenuDebugPanel : MonoBehaviour
 {
 
-    [DllImport("__Internal")]
-    private static extern string GetStringParams();
     public Text TestOutputComponentText;
     bool _visible = false;
     public void Toggle()
@@ -19,6 +17,5 @@ public class LoginMenuDebugPanel : MonoBehaviour
         else
             GetComponent<Animator>().SetTrigger("hide");
 
-        TestOutputComponentText.text = "_ " + GetStringParams();
     }
 }
