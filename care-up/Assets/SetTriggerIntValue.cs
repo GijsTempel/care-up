2714d62986d0c9a9d079f7ccf1133c6f27b942ba
PeterHarmatiy <47519815+PeterHarmatiy@users.noreply.github.com>
2021-12-09 20:32:16 +0200

    Restored scene's files

---
+++ care-up/Assets/SetTriggerIntValue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTriggerIntValue : StateMachineBehaviour
+{
+    public string TriggerName;
+    public int value;
+    public int actionFrame = 0;
+    protected float frame;
+    protected float prevFrame;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (actionFrame == 0)
+        {
+            animator.SetInteger(TriggerName, value);
+        }
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.speed != 0)
+        {
+            prevFrame = frame;
+            frame = stateInfo.normalizedTime * stateInfo.length;
+
+            if (PlayerAnimationManager.CompareFrames(frame, prevFrame, actionFrame))
+            {
+                animator.SetInteger(TriggerName, value);
+            }
+        }
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (actionFrame / 60f > frame)
+        {
+            animator.SetInteger(TriggerName, value);
+        }
+    }
+}
