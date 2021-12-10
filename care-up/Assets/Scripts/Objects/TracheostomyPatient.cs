cfbe9e5a0559ef0781dd6d1fef66c3cc72810fd5
PeterHarmatiy <47519815+PeterHarmatiy@users.noreply.github.com>
2021-12-10 11:43:53 +0200

    Restored missed animSeq xmls

---
+++ care-up/Assets/Scripts/Objects/TracheostomyPatient.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracheostomyPatient : PersonObject {
+
+    private Animator animator;
+    private Animator PlayerAnimator;
+    public Vector3 playerPosition;
+    public Vector3 playerRotation;
+    public Transform playerPositionTarget;
+
+    protected override void Start()
+    {
+        base.Start();
+        PlayerAnimator = GameObject.FindObjectOfType<PlayerAnimationManager>().GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+    }
+
+    public override void Talk(string topic = "", string audio = "")
+    {
+        if (ViewModeActive() || topic == "CM_Leave" || topic == "")
+            return;
+
+        if (actionManager.CompareTopic(topic))
+        {
+            switch (topic)
+            {
+                case "LayInHalfPosOnBed":
+                    /*PlayerAnimator.SetTrigger("StartSittingInBedAnimation");
+                    PlayerAnimator.SetTrigger("S StartSittingInBedAnimation");*/
+                    animator.SetTrigger("Patient_Oke");
+                    break;
+                case "Hello":
+
+                    PlayerAnimator.SetTrigger("Player_Dialog_Greeting");
+                    PlayerAnimator.SetTrigger("S Player_Dialog_Greeting");
+
+                    break;
+                default:
+                    break;
+            }
+            AttemptPlayAudioAfterTalk(audio);
+            NextDialogue();
+        }
+
+        actionManager.OnTalkAction(topic);
+    }
+}
