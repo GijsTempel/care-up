7733b69ad874b902ee34194a92dabeabb5f1604c
PeterHarmatiy <47519815+PeterHarmatiy@users.noreply.github.com>
2022-06-08 17:23:53 +0300

    Added script to look at player

---
+++ care-up/Assets/LookAtPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    GameObject _PlayerLookAtPoint; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        _PlayerLookAtPoint = GameObject.Find("PlayerLookAtPoint");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        transform.LookAt(_PlayerLookAtPoint.transform);
+    }
+}
