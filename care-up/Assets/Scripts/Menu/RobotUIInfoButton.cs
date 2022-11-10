dc5e51398b5f3ed61b30cd4c45decbee97c03cdc
Alex <olexandrzhytaryuk@gmail.com>
2022-11-10 13:20:45 +0200

    "removing" some usage of pdf in robotUI

--- care-up/Assets/Scripts/Menu/RobotUIInfoButton.cs
+++ care-up/Assets/Scripts/Menu/RobotUIInfoButton.cs
@@ -3,26 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RobotUIInfoButton : MonoBehaviour {
-    
-    private static Paroxe.PdfRenderer.PDFViewer pdf;
-    private Paroxe.PdfRenderer.PDFAsset asset;
 
-    private void Awake()
+    //private static Paroxe.PdfRenderer.PDFViewer pdf;
+    //private Paroxe.PdfRenderer.PDFAsset asset;
+
+    /*private void Awake()
     {
         if (pdf == null)
         {
             pdf = GameObject.FindObjectOfType<Paroxe.PdfRenderer.PDFViewer>();
         }
-    }
-    
+    }*/
+
     public void Set(string name)
     {
-        asset = Resources.Load<Paroxe.PdfRenderer.PDFAsset>("Prefabs\\UI\\" + name);
+        ObsoleteWarning();
+        //asset = Resources.Load<Paroxe.PdfRenderer.PDFAsset>("Prefabs\\UI\\" + name);
     }
 
     public void Toggle(bool value)
     {
-        pdf.PDFAsset = asset;
-        pdf.ReloadDocument();
+        ObsoleteWarning();
+        //pdf.PDFAsset = asset;
+        //pdf.ReloadDocument();
+    }
+
+    void ObsoleteWarning()
+    {
+        Debug.LogWarning("Nothing happens, obsolete element due to PDF renderer removal.");
     }
 }
