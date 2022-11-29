d5cd4c24601385dc709e2b5e633f8c12d446ded7
Acvarium <3dvits@gmail.com>
2022-11-29 17:04:46 +0200

    Updated WebGL Template

--- care-up/Assets/Scripts/GetServerAddr.cs
+++ care-up/Assets/Scripts/GetServerAddr.cs
@@ -8,8 +8,8 @@ public static class CAServer
     public static string DevBundleAddress = "https://careup.sharpminds.com/Addressables";//"https://ab.3dvit.in.ua/webgl/NewGraphicTest/StreamingAssets/aa/AddressablesLink";
     public static string Version = Application.version;
 
-    public static string BundleAddress = ReleaseBundleAddress;
-    //public static string BundleAddress = DevBundleAddress;
+    //public static string BundleAddress = ReleaseBundleAddress;
+    public static string BundleAddress = DevBundleAddress;
 
 }
 
