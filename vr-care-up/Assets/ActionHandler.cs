870190a45eea0d935050a907175b78d1373a3006
Acvarium <3dvits@gmail.com>
2023-07-06 00:46:54 +0300

    First action counted

--- vr-care-up/Assets/ActionHandler.cs
+++ vr-care-up/Assets/ActionHandler.cs
@@ -7,13 +7,13 @@ public class ActionHandler : MonoBehaviour
     ActionManager actionManager;
     public void TryExecuteAction(ActionManager.ActionType actionType, string leftHandObjectName, string rightHandObjectName)
     {
-        if (actionManager = null)
+        if (actionManager == null)
             actionManager = GameObject.FindObjectOfType<ActionManager>();
         if (actionManager == null)
             return;
         if (actionType == ActionManager.ActionType.ObjectUse)
         {
-            actionManager.CompareUseObject(leftHandObjectName);
+            actionManager.OnUseAction(leftHandObjectName);
         }
 
     }
