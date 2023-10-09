f0804ee6a75cc93e9da0fd8e949418833cff1d8d
Acvarium <3dvits@gmail.com>
2023-10-09 16:30:50 +0300

    Fixed some logic

--- vr-care-up/Assets/PlayAudioOnFrame.cs
+++ vr-care-up/Assets/PlayAudioOnFrame.cs
@@ -10,42 +10,62 @@ public class PlayAudioOnFrame : StateMachineBehaviour
     protected float frame = 0f;
     protected float prevFrame = 0f;
 
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        frame = 0f;
+        prevFrame = 0f;
+
+        if (audioFrame == 0)
+        {
+            Execute();
+        }
+    }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {     
-        if (PlayerAnimationManager.CompareFrames(frame, prevFrame, audioFrame))
-        {
-            if (controlObjectName == "")
-            {
-                if (GameObject.Find(audioObjectName) != null)
-                {
-                    GameObject.Find(audioObjectName).GetComponent<AudioSource>().Play();
-                }
-            }
-            else
-            {
-                GameObject controlObject = GameObject.Find(controlObjectName);
-                if (controlObject != null)
-                {
-                    if (controlObject.GetComponent<ShowHideObjects>() != null)
-                    {
-                        foreach(GameObject g in controlObject.GetComponent<ShowHideObjects>().hidenObjects)
-                        {
-                            if (g.name == audioObjectName && g.GetComponent<AudioSource>() != null)
-                            {
-                                g.GetComponent<AudioSource>().Play();
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
+    {
         if (animator.speed != 0)
         {
             prevFrame = frame;
             frame = stateInfo.normalizedTime * stateInfo.length;
+
+            if (PlayerAnimationManager.CompareFrames(frame, prevFrame, audioFrame))
+            {
+                Execute();
+            }
+        }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (audioFrame < 0 || (audioFrame / 60f > frame))
+        {
+            Execute();
+        }
+    }
+
+    void Execute()
+    {
+        if (controlObjectName == "")
+        {
+            if (GameObject.Find(audioObjectName) != null)
+            {
+                GameObject.Find(audioObjectName).GetComponent<AudioSource>().Play();
+            }
+        }
+        else
+        {
+            GameObject controlObject = GameObject.Find(controlObjectName);
+            if (controlObject != null && controlObject.GetComponent<ShowHideObjects>() != null)
+            {
+                foreach (GameObject g in controlObject.GetComponent<ShowHideObjects>().hidenObjects)
+                {
+                    if (g.name == audioObjectName && g.GetComponent<AudioSource>() != null)
+                    {
+                        g.GetComponent<AudioSource>().Play();
+                        break;
+                    }
+                }
+            }
         }
     }
 }
