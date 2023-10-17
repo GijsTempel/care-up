8cfe10e7e736095ca6db2a6fee9236c336405418
Vitalii Shmorhun <3dvits@gmail.com>
2023-10-17 18:44:11 +0300

    Basis for steps window

--- care-up/Assets/Scripts/Player/GameUI.cs
+++ care-up/Assets/Scripts/Player/GameUI.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
 {
+
+    public GameObject subjectWindow;
     string autoPlayMoveWTGName = "";
     public AnimatedFingerHint animatedFinger;
     GameObject Player;
@@ -507,6 +509,11 @@ public class GameUI : MonoBehaviour
         GameObject.Find("theoryPanel/panel/quizElements/Continue").gameObject.GetComponent<Button>().onClick.AddListener(
             () => GameObject.FindObjectOfType<PlayerScript>().CloseRobotUI());
         theoryTab.Show(false);
+    }
+
+    public void ShowSubjectWindow(bool toShow = true)
+    {
+        subjectWindow.SetActive(toShow);
     }
 
     // Use this for initialization
