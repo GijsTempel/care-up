5be5b1f5616f61a33c79ec05515f8228843be993
Vitalii Shmorhun <3dvits@gmail.com>
2024-01-03 01:34:53 +0200

    Improved debug screen. Worked on issues with the scene steps

--- vr-care-up/Assets/Scripts/UI/DebugDisplay.cs
+++ vr-care-up/Assets/Scripts/UI/DebugDisplay.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,12 +9,26 @@ public class DebugDisplay : MonoBehaviour
     Dictionary<string, string> debugLogs = new Dictionary<string, string>();
 
 
-    public Text display;
+    public GameObject displayObject;
     public string marker = "";
+    Text displayTextUnit;
+    TextMeshProUGUI displayTextMeshUnit;
+
+
+    int errorCount = 0;
 
     void OnEnable()
     {
         Application.logMessageReceived += HandleLog;
+        
+    }
+
+    void Start()
+    {
+        if (displayObject.GetComponent<Text>() != null)
+            displayTextUnit = displayObject.GetComponent<Text>();
+        else if (displayObject.GetComponent<TextMeshProUGUI>())
+            displayTextMeshUnit = displayObject.GetComponent<TextMeshProUGUI>();
     }
 
     void OnDisable()
@@ -21,11 +36,17 @@ public class DebugDisplay : MonoBehaviour
         Application.logMessageReceived -= HandleLog;
     }
 
+
     void HandleLog (string logString, string stackTrace, LogType type)
     {
         if (marker != ""){
             if (!logString.Contains(marker))
                 return;
+        }
+        if (type == LogType.Error)
+        {
+            debugLogs.Add("Error" + errorCount.ToString(), logString);
+            errorCount++;
         }
         if (type == LogType.Log) 
         {
@@ -37,17 +58,27 @@ public class DebugDisplay : MonoBehaviour
                 debugLogs[debugKey] = debugValue;
             else
                 debugLogs.Add(debugKey, debugValue);
-
         }
        
         string displayText = "";
         foreach (KeyValuePair<string, string> log in debugLogs) {
-            if (log.Value == "")
-                displayText += log.Key + "\n";
-            else    
-                displayText += log.Key + ": " + log.Value + "\n";
+            string colorBegin = "";
+            string colorEnd = "";
+            if (displayTextMeshUnit != null)
+            {
+                colorEnd = "</color>";
+                if (log.Key.Contains("Error"))
+                    colorBegin = "<color=\"red\">";
+                else
+                    colorBegin = "<color=\"white\">";
+            }
+            
+            displayText += colorBegin + log.Key + ": " + log.Value + colorEnd + "\n";
             
         }
-        display.text = displayText;
+        if (displayTextUnit != null)
+            displayTextUnit.text = displayText;
+        else if (displayTextMeshUnit != null)
+            displayTextMeshUnit.text = displayText;
     }
 }
