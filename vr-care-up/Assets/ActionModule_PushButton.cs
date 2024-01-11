2edbd6b0093b51c08af18b53775d9b93338307e5
Vitalii Shmorhun <3dvits@gmail.com>
2024-01-12 00:18:34 +0200

    Some more improvements to the push button logic

--- vr-care-up/Assets/ActionModule_PushButton.cs
+++ vr-care-up/Assets/ActionModule_PushButton.cs
@@ -8,63 +8,61 @@ public class ActionModule_PushButton : MonoBehaviour
     public Transform rayPointerTransform;
     public GameObject buttonTop;
     public Transform maxPushTransform;
-    bool pushRestarted = false;
     float maxPushDist;
     public ActionTrigger actionTrigger;
 
     Vector3 rayDirection;
-    Vector3 originalPosition;
+    Vector3 buttonOriginalPosition;
+    Vector3 buttonOffset;
     float maxRayDist = 0.13f;
     bool pressed = false;
 
     void Start()
     {
-        originalPosition = buttonTop.transform.position;
+        buttonOriginalPosition = buttonTop.transform.position;
+        buttonOffset = buttonOriginalPosition - rayPointerTransform.position;
         rayDirection = (rayPointerTransform.position - transform.position).normalized;
-        Debug.Log(rayDirection);
         maxRayDist = Vector3.Distance(transform.position, rayPointerTransform.position);
         maxPushDist = Vector3.Distance(rayPointerTransform.position, maxPushTransform.position);
     }
 
-    void Update()
+    void FixedUpdate()
     {
         bool wasPressed = pressed;
-        Color c = Color.green;
-        // Vector3 topRayPosition = 
+        var topRayPos = buttonTop.transform.position - buttonOffset - rayDirection * 0.01f;
         
+        Color mainRayColor = Color.green;
+        Color topRayColor = Color.blue;
+        bool isTopRayColliding = Physics.Raycast(topRayPos, rayDirection, maxRayDist * 0.3f, layerMask);
+        if (isTopRayColliding)
+            topRayColor = Color.yellow;
         if (Physics.Raycast(transform.position, rayDirection, out RaycastHit hit, maxRayDist, layerMask))
         {
-            c = Color.red;
-
-            if (!pressed)
+            mainRayColor = Color.red;
+            Vector3 positionShiftVec = rayDirection.normalized * (maxRayDist - hit.distance);
+            if (isTopRayColliding)
             {
-                if (!pushRestarted && hit.distance / maxRayDist > 0.85f)
-                    pushRestarted = true;
-                // if (pushRestarted)
-                // {
-                    Vector3 positionShiftVec = rayDirection.normalized * (maxRayDist - hit.distance); ;
-                    Vector3 nextPosition = originalPosition - positionShiftVec;
-                    float pushDot = Vector3.Dot(((rayPointerTransform.position - positionShiftVec) -
-                        maxPushTransform.position).normalized, rayDirection);
-                    if (pushDot < 0)
-                    {
-                        nextPosition = originalPosition - rayDirection.normalized * maxPushDist;
-                        pressed = true;
-                    }
-                    buttonTop.transform.position = nextPosition;
-                // }
+                Vector3 nextButtonPosition = buttonOriginalPosition - positionShiftVec;
+                float pushDot = Vector3.Dot(((rayPointerTransform.position - positionShiftVec) -
+                    maxPushTransform.position).normalized, rayDirection);
+                if (pushDot < 0)
+                {
+                    nextButtonPosition = buttonOriginalPosition - rayDirection.normalized * maxPushDist;
+                    pressed = true;
+                }
+                buttonTop.transform.position = nextButtonPosition;
             }
         }
         else
         {
-            if (Vector3.Distance(originalPosition, buttonTop.transform.position) < 0.01f)
+            if (Vector3.Distance(buttonOriginalPosition, buttonTop.transform.position) < 0.02f)
                 pressed = false;
-            buttonTop.transform.position = Vector3.Lerp(buttonTop.transform.position, originalPosition, Time.deltaTime * 10f);
+            buttonTop.transform.position = Vector3.Lerp(buttonTop.transform.position, buttonOriginalPosition, Time.deltaTime * 10f);
         }
-        Debug.DrawRay(transform.position, rayDirection * maxRayDist, c, 0.0f, false); 
+        Debug.DrawRay(transform.position, rayDirection * maxRayDist, mainRayColor, 0.0f, false);
+        Debug.DrawRay(topRayPos + new Vector3(0.001f, 0, 0.001f), rayDirection * maxRayDist * 0.3f, topRayColor, 0.0f, false);
         if(pressed && !wasPressed)
         {
-            pushRestarted = false;
             if (actionTrigger != null)
                 actionTrigger.AttemptTrigger();
             Debug.Log("pressed");
