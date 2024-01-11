1eaeaaf82c1de6de37ef2ff4f89f89b81a4dabdb
Vitalii Shmorhun <3dvits@gmail.com>
2024-01-11 18:37:53 +0200

    Some more improvements to the push button. Changes to the model of hands cleaner

--- vr-care-up/Assets/ActionModule_PushButton.cs
+++ vr-care-up/Assets/ActionModule_PushButton.cs
@@ -15,47 +15,52 @@ public class ActionModule_PushButton : MonoBehaviour
     Vector3 rayDirection;
     Vector3 originalPosition;
     float maxRayDist = 0.13f;
-    Vector3 originPos;
     bool pressed = false;
+
     void Start()
     {
         originalPosition = buttonTop.transform.position;
         rayDirection = (rayPointerTransform.position - transform.position).normalized;
+        Debug.Log(rayDirection);
         maxRayDist = Vector3.Distance(transform.position, rayPointerTransform.position);
-        maxPushDist = Vector3.Distance(transform.position, maxPushTransform.position);
+        maxPushDist = Vector3.Distance(rayPointerTransform.position, maxPushTransform.position);
     }
 
     void Update()
     {
         bool wasPressed = pressed;
-        bool moveDown = false;
-        if (!pressed && Physics.Raycast(transform.position, rayDirection, out RaycastHit hit, maxRayDist, layerMask))
+        if (Physics.Raycast(transform.position, rayDirection, out RaycastHit hit, maxRayDist, layerMask))
         {
-            if (!pushRestarted && hit.distance / maxRayDist > 0.9f)
-                pushRestarted = true;
-            if (pushRestarted)
+            if (!pressed)
             {
-                moveDown = true;
-                Vector3 nextPosition = originalPosition - rayDirection.normalized * (maxRayDist - hit.distance);
-                float pushDot = Vector3.Dot((nextPosition - maxPushTransform.position).normalized, rayDirection);
-                if (pushDot < 0)
-                {
-                    nextPosition = originalPosition - rayDirection.normalized * maxPushDist;
-                    pressed = true;
-                }
-                buttonTop.transform.position = nextPosition;
+                if (!pushRestarted && hit.distance / maxRayDist > 0.85f)
+                    pushRestarted = true;
+                // if (pushRestarted)
+                // {
+                    Vector3 positionShiftVec = rayDirection.normalized * (maxRayDist - hit.distance); ;
+                    Vector3 nextPosition = originalPosition - positionShiftVec;
+                    float pushDot = Vector3.Dot(((rayPointerTransform.position - positionShiftVec) -
+                        maxPushTransform.position).normalized, rayDirection);
+                    if (pushDot < 0)
+                    {
+                        nextPosition = originalPosition - rayDirection.normalized * maxPushDist;
+                        pressed = true;
+                    }
+                    buttonTop.transform.position = nextPosition;
+                // }
             }
         }
-        if (!moveDown)
+        else
         {
-            buttonTop.transform.position = Vector3.Lerp(buttonTop.transform.position, originalPosition, Time.deltaTime * 10f);
-            if (Vector3.Distance(originalPosition, buttonTop.transform.position) < 0.00001f)
+            if (Vector3.Distance(originalPosition, buttonTop.transform.position) < 0.01f)
                 pressed = false;
+            buttonTop.transform.position = Vector3.Lerp(buttonTop.transform.position, originalPosition, Time.deltaTime * 10f);
         }
         if(pressed && !wasPressed)
         {
             pushRestarted = false;
-            actionTrigger.AttemptTrigger();
+            if (actionTrigger != null)
+                actionTrigger.AttemptTrigger();
             Debug.Log("pressed");
         }
     }
