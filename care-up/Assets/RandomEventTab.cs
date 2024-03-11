234e0e13e0b886f373ef3348d673dc9d11302278
Acvarium <3dvits@gmail.com>
2024-03-11 16:11:54 +0200

    Extended debug cheat mode

--- care-up/Assets/RandomEventTab.cs
+++ care-up/Assets/RandomEventTab.cs
@@ -276,7 +276,7 @@ public class RandomEventTab : MonoBehaviour
                 answerButtons[i].gameObject.SetActive(true);
                 string buttonText = currentQuestion.answers[shuffledIndexes[i]].text;
                 string cheatSimbol = "";
-#if UNITY_EDITOR
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
                 ObjectsIDsController objectsIDsController = GameObject.FindAnyObjectByType<ObjectsIDsController>();
                 if (objectsIDsController != null && objectsIDsController.cheat)
                 {
